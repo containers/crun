387c254807ee51db711e2eb8f1ec38278376231e
Giuseppe Scrivano <giuseppe@scrivano.org>
2017-07-05 12:44:10 +0200

    make: add infrastructure for make syntax-check
    Files copied from GNU gnulib.
    
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

---
+++ cfg.mk
@@ -0,0 +1,20 @@
+export VC_LIST_EXCEPT_DEFAULT=^(lib/.*|m4/.*|md5/.*|build-aux/.*|src/gettext\.h|.*ChangeLog)$$
+
+local-checks-to-skip = \
+    sc_immutable_NEWS \
+    sc_copyright_check \
+    \
+    sc_prohibit_strcmp \
+    sc_program_name \
+    sc_bindtextdomain	 \
+    sc_error_message_period \
+    sc_unmarked_diagnostics \
+    sc_prohibit_always_true_header_tests \
+    sc_prohibit_intprops_without_use \
+
+
+#SHELL=bash -x
+show-vc-list-except:
+	@$(VC_LIST_EXCEPT)
+
+VC_LIST_ALWAYS_EXCLUDE_REGEX = ^ABOUT-NLS|maint.mk|git.mk|tests.*|COPYING$$
