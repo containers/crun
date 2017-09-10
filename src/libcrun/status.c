7c867e34ce4c2d7bfd39ea3635ebe5ffd9e05545
Giuseppe Scrivano <giuseppe@scrivano.org>
2017-09-10 19:29:35 +0200

    libcrun: change license to LGPL3+
    The world is not perfect yet.
    
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/libcrun/status.c
+++ src/libcrun/status.c
@@ -3,16 +3,16 @@
  *
  * Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
  * libocispec is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * libocispec is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with crun.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define _GNU_SOURCE
