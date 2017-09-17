a92dd207e9e486af291b7799e75d5167370ba6fe
Giuseppe Scrivano <giuseppe@scrivano.org>
2017-09-17 21:22:06 +0200

    tests: add tests for rlimits
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

---
+++ tests/test_rlimits.py
@@ -0,0 +1,72 @@
+#!/bin/python -Es
+# crun - OCI runtime written in C
+#
+# Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
+# libocispec is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# libocispec is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with crun.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+import json
+import subprocess
+import os
+import shutil
+import sys
+from tests_utils import *
+
+def parse_proc_limits(content):
+    lines = content.split("\n")
+    r = {}
+    mappings = {'Max open files' : "RLIMIT_NOFILE",
+                'Max processes' : "RLIMIT_PROC",
+                'Max cpu time' : "RLIMIT_CPU",
+                'Max pending signals' : "RLIMIT_SIGPENDING"}
+    for i in lines[1:-1]:
+        s = [x.strip() for x in i.split("  ") if x != ""]
+        r[mappings.get(s[0], s[0])] = s
+    return r
+
+def test_rlimits():
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'cat', '/proc/self/limits']
+    rlimits = [
+        {"type" : "RLIMIT_SIGPENDING",
+         "soft" : 1000,
+         "hard" : 2000},
+        {"type" : "RLIMIT_PROC",
+         "soft" : 50,
+         "hard" : 100},
+        {"type" : "RLIMIT_NOFILE",
+         "soft" : 512,
+         "hard" : 512},
+        {"type" : "RLIMIT_CPU",
+         "soft" : 2,
+         "hard" : 3},
+    ]
+    conf['process']['rlimits'] = rlimits
+    add_all_namespaces(conf)
+    out = run_and_get_output(conf)
+    limits = parse_proc_limits(out)
+
+    for v in rlimits:
+        limit = limits.get(v['type'])
+        if str(limit[1]) != str(v['soft']) or str(limit[2]) != str(v['hard']):
+            sys.stderr.write("%s: %s %s\n" % (limit[0], limit[1], limit[2]))
+            return -1
+    return 0
+
+all_tests = {
+    "rlimits" : test_rlimits,
+}
+
+if __name__ == "__main__":
+    tests_main(all_tests)
