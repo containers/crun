9217731d7efce978dfd2a370848c57a842b4aaa0
Giuseppe Scrivano <giuseppe@scrivano.org>
2017-10-30 11:56:45 +0100

    container: do not wait for the container on create
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/libcrun/container.c
+++ src/libcrun/container.c
@@ -720,7 +720,8 @@ libcrun_container_run_internal (libcrun_container *container, struct libcrun_con
     }
 
   ret = wait_for_process (pid, context, terminal_fd, notify_socket, err);
-  cleanup_watch (context, context->id, terminal_fd, context->stderr);
+  if (! context->has_fifo_exec_wait)
+    cleanup_watch (context, context->id, terminal_fd, context->stderr);
   return ret;
 }
 
