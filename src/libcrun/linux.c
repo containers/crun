f1d79db93913ee8bda96477e879b02a0d98ec8e0
Giuseppe Scrivano <giuseppe@scrivano.org>
2017-11-10 14:04:57 +0100

    linux: change error message for PR_SET_KEEPCAPS failure
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/libcrun/linux.c
+++ src/libcrun/linux.c
@@ -1078,7 +1078,7 @@ libcrun_set_caps (libcrun_container *container, int keep_setuid, libcrun_error_t
     }
   ret = prctl (PR_SET_KEEPCAPS, 1, 0, 0, 0);
   if (UNLIKELY (ret < 0))
-    return crun_make_error (err, errno, "PR_SET_KEEPCAPS");
+    return crun_make_error (err, errno, "error while setting PR_SET_KEEPCAPS");
 
   return set_required_caps (&caps, def->process->no_new_privileges, err);
 }
