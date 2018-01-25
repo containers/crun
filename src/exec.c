bd4d675186d6fd2700b0f944efdc47c7c7f6bb64
Giuseppe Scrivano <giuseppe@scrivano.org>
2018-01-25 11:04:42 +0100

    src: fix copyright project name
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/exec.c
+++ src/exec.c
@@ -2,12 +2,12 @@
  * crun - OCI runtime written in C
  *
  * Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
- * libocispec is free software; you can redistribute it and/or modify
+ * crun is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * libocispec is distributed in the hope that it will be useful,
+ * crun is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
