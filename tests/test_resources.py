fa7384d249c89eca56934f01ae83196fac2e97cb
Giuseppe Scrivano <giuseppe@scrivano.org>
2018-02-11 10:34:36 +0100

    tests: add test for pids limit
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

---
+++ tests/test_resources.py
@@ -0,0 +1,41 @@
+#!/bin/env $PYTHON
+# crun - OCI runtime written in C
+#
+# Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
+# crun is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# crun is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with crun.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+import json
+import subprocess
+import os
+import shutil
+import sys
+from tests_utils import *
+
+def test_resources_pid_limit():
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'cat', "/sys/fs/cgroup/pids/pids.max"]
+    conf['linux']['resources'] = {"pids" : {"limit" : 1024}}
+    add_all_namespaces(conf)
+    out, _ = run_and_get_output(conf)
+    if "1024" not in out:
+        return -1
+    return 0
+    
+all_tests = {
+    "resources-pid-limit" : test_resources_pid_limit,
+}
+
+if __name__ == "__main__":
+    tests_main(all_tests)
