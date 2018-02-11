c1042e80ba7f87b7de04d8158de51802ab94783d
Giuseppe Scrivano <giuseppe@scrivano.org>
2018-02-11 15:24:10 +0100

    tests: basic tests for exec
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

---
+++ tests/test_exec.py
@@ -0,0 +1,87 @@
+#!/bin/env $PYTHON
+# crun - OCI runtime written in C
+#
+# Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
+# crun is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# crun is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with crun.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+import json
+import subprocess
+import os
+import shutil
+import sys
+from tests_utils import *
+
+def test_exec():
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'pause']
+    add_all_namespaces(conf)
+    cid = None
+    try:
+        proc, cid = run_and_get_output(conf, command='run', use_popen=True)
+        for i in range(50):
+            try:
+                s = run_crun_command(["state", cid])
+                break
+            except Exception as e:
+                time.sleep(0.1)
+
+        out = run_crun_command(["exec", cid, "/init", "echo", "foo"])
+        if "foo" not in out:
+            return -1
+    finally:
+        if cid is not None:
+            run_crun_command(["delete", "-f", cid])
+    return 0
+
+def test_exec_not_exists_helper(detach):
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'pause']
+    add_all_namespaces(conf)
+    cid = None
+    try:
+        proc, cid = run_and_get_output(conf, command='run', use_popen=True)
+        for i in range(50):
+            try:
+                s = run_crun_command(["state", cid])
+                break
+            except Exception as e:
+                time.sleep(0.1)
+
+        try:
+            if detach:
+                out = run_crun_command(["exec", "-d", cid, "/not.here"])
+            else:
+                out = run_crun_command(["exec", cid, "/not.here"])
+        except Exception as e:
+            return 0
+    finally:
+        if cid is not None:
+            run_crun_command(["delete", "-f", cid])
+    return 1
+
+def test_exec_not_exists():
+    return test_exec_not_exists_helper(False)
+
+def test_exec_detach_not_exists():
+    return test_exec_not_exists_helper(False)
+    
+all_tests = {
+    "exec" : test_exec,
+    "exec-not-exists" : test_exec_not_exists,
+    "exec-detach-not-exists" : test_exec_detach_not_exists,
+}
+
+if __name__ == "__main__":
+    tests_main(all_tests)
