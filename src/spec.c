9a63f8eaf597a855ece8f9d6f8e108dc8672a590
Giuseppe Scrivano <giuseppe@scrivano.org>
2018-02-22 22:23:32 +0100

    spec: properly propagate error message
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/spec.c
+++ src/spec.c
@@ -260,7 +260,7 @@ crun_command_spec (struct crun_global_arguments *global_args, int argc, char **a
   if (ret < 0)
     return ret;
   if (ret)
-    crun_make_error (err, 0, "config.json already exists", err);
+    return crun_make_error (err, 0, "config.json already exists", err);
 
   ret = write_file ("config.json", spec_file, sizeof (spec_file), err);
   if (ret < 0)
