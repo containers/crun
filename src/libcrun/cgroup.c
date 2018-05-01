d09a1d79f02edb01dc4374e463034f929bee7d07
Giuseppe Scrivano <giuseppe@scrivano.org>
2018-05-01 14:10:21 +0200

    cgroup: don't require write access when joining a cgroup
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/libcrun/cgroup.c
+++ src/libcrun/cgroup.c
@@ -205,7 +205,7 @@ enter_cgroup (pid_t pid, const char *path, int ensure_missing, libcrun_error_t *
     {
       char subsystem_path[64];
       sprintf (subsystem_path, "/sys/fs/cgroup/%s", subsystems[i]);
-      ret = crun_path_exists (subsystem_path, 0, err);
+      ret = crun_path_exists (subsystem_path, !ensure_missing, err);
       if (UNLIKELY (ret < 0))
         return ret;
       if (ret == 0)
