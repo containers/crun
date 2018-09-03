1a1063cc38ba472cdfd9495cb0b51cd616a81815
Giuseppe Scrivano <giuseppe@scrivano.org>
2018-09-03 13:46:44 +0200

    exec: fix realloc size
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- src/exec.c
+++ src/exec.c
@@ -67,7 +67,7 @@ static char args_doc[] = "exec CONTAINER cmd";
 static void
 append_env (const char *arg)
 {
-  exec_options.env = realloc (exec_options.env, exec_options.env_size + 2);
+  exec_options.env = realloc (exec_options.env, (exec_options.env_size + 2) * sizeof (*exec_options.env));
   if (exec_options.env == NULL)
     error (EXIT_FAILURE, errno, "cannot allocate memory");
   exec_options.env[exec_options.env_size + 1] = NULL;
