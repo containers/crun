57a02dfbc80feaae97ec884a260475591f003ce0
Giuseppe Scrivano <giuseppe@scrivano.org>
2019-01-01 21:29:59 +0100

    src: update copyright years
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- python/crun_python.c
+++ python/crun_python.c
@@ -1,7 +1,7 @@
 /*
  *crun - OCI runtime written in C
  *
- *Copyright (C) 2018 Giuseppe Scrivano <giuseppe@scrivano.org>
+ *Copyright (C) 2018, 2019 Giuseppe Scrivano <giuseppe@scrivano.org>
  *crun is free software; you can redistribute it and/or modify
  *it under the terms of the GNU Lesser General Public License as published by
  *the Free Software Foundation; either version 3 of the License, or
