558b82ce72f039cdc6d86f387e10f53cac93678e
Giuseppe Scrivano <giuseppe@scrivano.org>
2019-02-01 11:37:01 +0100

    autogen.sh: fail early if pkg-config is not found
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+if ! (pkg-config --version > /dev/null 2>&1); then
+    echo "pkg-config not found.  Please install it">&2
+    exit 1
+fi
+
 git submodule update --init --recursive
+
 exec autoreconf -fis
