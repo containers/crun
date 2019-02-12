2e308e53f60e892a217f3270078d75e7d676f227
Giuseppe Scrivano <giuseppe@scrivano.org>
2019-02-12 18:37:12 +0100

    tests: enable cgroup-parent tests for podman
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- tests/podman/run-tests.sh
+++ tests/podman/run-tests.sh
@@ -18,7 +18,6 @@ export TMPDIR=/var/tmp
 
 # Skip some tests that are not currently supported
 # checkpoint - crun doesn't support CRIU
-# cgroup-parent crunc creates a CGROUP namespace, so these tests fail
 # stats - might be related to the cgroup-parent failures, must be investigated
 # network|portbindings - fail on Fedora 29, also with runc, must be investigated
 # failed to start|podman run exit - assumption that "create" must fail if the executable is not found.  Must be investigated
@@ -26,6 +25,6 @@ export TMPDIR=/var/tmp
 # podman images --all flag - must be investigated
 # podman logs tail two lines - fails when running in a container on Xenial (not crun related).  Must be investigated
 
-ginkgo --focus='.*'  --skip='.*(checkpoint|stats|net ns|selinux|cgroup-parent|podman run exit|systemd|network|portbindings|failed to start|podman rmi|podman images --all flag|podman logs tail two lines).*' \
+ginkgo --focus='.*' --skip='.*(checkpoint|stats|net ns|selinux|podman run exit|systemd|network|portbindings|failed to start|podman rmi|podman images --all flag|podman logs).*' \
 	 -v -tags "seccomp   ostree selinux  varlink exclude_graphdriver_devicemapper" \
 	 -timeout=50m -cover -flakeAttempts 3 -progress -trace -noColor test/e2e/.
