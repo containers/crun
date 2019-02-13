adf6342c8e06a509c1c739d3e1d6115f14d7b774
Giuseppe Scrivano <giuseppe@scrivano.org>
2019-02-13 21:05:59 +0100

    tests, podman: enable more tests
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- tests/podman/run-tests.sh
+++ tests/podman/run-tests.sh
@@ -16,15 +16,20 @@ export STORAGE_OPTIONS="--storage-driver=vfs"
 ulimit -u unlimited
 export TMPDIR=/var/tmp
 
-# Skip some tests that are not currently supported
-# checkpoint - crun doesn't support CRIU
-# stats - might be related to the cgroup-parent failures, must be investigated
-# network|portbindings - fail on Fedora 29, also with runc, must be investigated
-# failed to start|podman run exit - assumption that "create" must fail if the executable is not found.  Must be investigated
-# podman rmi - must be investigated
-# podman images --all flag - must be investigated
-# podman logs tail two lines - fails when running in a container on Xenial (not crun related).  Must be investigated
+# Skip some tests that are not currently supported:
+#
+# - checkpoint
+#  crun doesn't support CRIU.
+#
+# - selinux
+#   Travis runs on Ubuntu.
+#
+# - systemd
+#   must fix NOTIFY_PATH support in crun.  upstream runc doesn't support it as well :-) https://github.com/opencontainers/runc/pull/1807
+#
+# - podman run exit 12*|podman run exit code on failure to exec|failed to start
+#   assumption that "create" must fail if the executable is not found.  We must add lookup for the executable in $PATH to mimic the runc behavior.
 
-ginkgo --focus='.*' --skip='.*(checkpoint|stats|net ns|selinux|podman run exit|systemd|network|portbindings|failed to start|podman rmi|podman images --all flag|podman logs).*' \
+ginkgo --focus='.*' --skip='.*(checkpoint|selinux|systemd|podman run exit 12*|podman run exit code on failure to exec|failed to start).*' \
 	 -v -tags "seccomp   ostree selinux  varlink exclude_graphdriver_devicemapper" \
 	 -timeout=50m -cover -flakeAttempts 3 -progress -trace -noColor test/e2e/.
