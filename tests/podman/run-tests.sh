030c3cae81258210dcaf5ba6a371a52655d7d5d1
Giuseppe Scrivano <giuseppe@scrivano.org>
2019-03-09 09:40:45 +0100

    tests: update the version of podman
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

--- tests/podman/run-tests.sh
+++ tests/podman/run-tests.sh
@@ -21,6 +21,9 @@ export TMPDIR=/var/tmp
 # - checkpoint
 #  crun doesn't support CRIU.
 #
+# - search|trust|inspect|logs|generate|import
+#   Flaky or not using the runtime.
+#
 # - selinux
 #   Travis runs on Ubuntu.
 #
@@ -30,6 +33,6 @@ export TMPDIR=/var/tmp
 # - podman run exit 12*|podman run exit code on failure to exec|failed to start
 #   assumption that "create" must fail if the executable is not found.  We must add lookup for the executable in $PATH to mimic the runc behavior.
 
-ginkgo --focus='.*' --skip='.*(checkpoint|selinux|systemd|podman run exit 12*|podman run exit code on failure to exec|failed to start).*' \
+ginkgo --focus='.*' --skip='.*(checkpoint|selinux|systemd|podman run exit 12*|podman run exit code on failure to exec|failed to start|search|trust|inspect|logs|generate|import).*' \
 	 -v -tags "seccomp   ostree selinux  varlink exclude_graphdriver_devicemapper" \
 	 -timeout=50m -cover -flakeAttempts 3 -progress -trace -noColor test/e2e/.
