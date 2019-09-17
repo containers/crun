containers/crun

commit 83a2975038e09d5830ee7e164b95ab939742c334

container: move get_executable before installing seccomp
Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

src/libcrun/container.c
@@ -441,7 +441,8 @@ int unblock_signals (libcrun_error_t *err)
 
 static int
 container_entrypoint_init (void *args, const char *notify_socket,
-                           int sync_socket, libcrun_error_t *err)
+                           int sync_socket, const char **exec_path,
+                           libcrun_error_t *err)
 {
   struct container_entrypoint_s *entrypoint_args = args;
   libcrun_container_t *container = entrypoint_args->container;
@@ -494,6 +495,20 @@ container_entrypoint_init (void *args, const char *notify_socket,
   ret = libcrun_do_pivot_root (container, entrypoint_args->context->no_pivot, rootfs, err);
   if (UNLIKELY (ret < 0))
     return ret;
+
+  if (def->process && def->process->args)
+    {
+      *exec_path = find_executable (def->process->args[0]);
+      if (UNLIKELY (*exec_path == NULL))
+        {
+          if (errno == ENOENT)
+            ret = crun_make_error (err, errno, "executable file not found in $PATH");
+          else
+            ret = crun_make_error (err, errno, "open executable");
+          sync_socket_write_error (sync_socket, err);
+          return ret;
+        }
+    }
 
   if (has_terminal)
     {
@@ -595,7 +610,7 @@ container_entrypoint (void *args, const char *notify_socket,
 
   crun_set_output_handler (log_write_to_sync_socket, args);
 
-  ret = container_entrypoint_init (args, notify_socket, sync_socket, err);
+  ret = container_entrypoint_init (args, notify_socket, sync_socket, &exec_path, err);
   if (UNLIKELY (ret < 0))
     {
       /* If it fails to write the error using the sync socket, then fallback
@@ -612,20 +627,6 @@ container_entrypoint (void *args, const char *notify_socket,
   ret = unblock_signals (err);
   if (UNLIKELY (ret < 0))
     return ret;
-
-  if (def->process && def->process->args)
-    {
-      exec_path = find_executable (def->process->args[0]);
-      if (UNLIKELY (exec_path == NULL))
-        {
-          if (errno == ENOENT)
-            ret = crun_make_error (err, errno, "executable file not found in $PATH");
-          else
-            ret = crun_make_error (err, errno, "open executable");
-          sync_socket_write_error (sync_socket, err);
-          return ret;
-        }
-    }
 
   ret = sync_socket_send_sync (sync_socket, false, err);
   if (UNLIKELY (ret < 0))

commit 4f92eb59152ffe7776e82611924a852a4e19ebb9

linux: do not write twice the same error
regression introduced with 83a2975038e09d5830ee7e164b95ab939742c334

Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

src/libcrun/container.c
@@ -496,17 +496,27 @@ container_entrypoint_init (void *args, const char *notify_socket,
   if (UNLIKELY (ret < 0))
     return ret;
 
+  if (clearenv ())
+    return crun_make_error (err, errno, "clearenv");
+
+  if (def->process)
+    {
+      size_t i;
+
+      for (i = 0; i < def->process->env_len; i++)
+        if (putenv (def->process->env[i]) < 0)
+          return crun_make_error (err, errno, "putenv '%s'", def->process->env[i]);
+    }
+
   if (def->process && def->process->args)
     {
       *exec_path = find_executable (def->process->args[0]);
       if (UNLIKELY (*exec_path == NULL))
         {
           if (errno == ENOENT)
-            ret = crun_make_error (err, errno, "executable file not found in $PATH");
-          else
-            ret = crun_make_error (err, errno, "open executable");
-          sync_socket_write_error (sync_socket, err);
-          return ret;
+            return crun_make_error (err, errno, "executable file not found in $PATH");
+
+          return crun_make_error (err, errno, "open executable");
         }
     }
 
@@ -575,17 +585,6 @@ container_entrypoint_init (void *args, const char *notify_socket,
     if (UNLIKELY (chdir (def->process->cwd) < 0))
       return crun_make_error (err, errno, "chdir");
 
-  if (clearenv ())
-    return crun_make_error (err, errno, "clearenv");
-
-  if (def->process)
-    {
-      size_t i;
-
-      for (i = 0; i < def->process->env_len; i++)
-        if (putenv (def->process->env[i]) < 0)
-          return crun_make_error (err, errno, "putenv '%s'", def->process->env[i]);
-    }
   if (notify_socket)
     {
       char *notify_socket_env;
@@ -1986,6 +1985,17 @@ libcrun_container_exec (libcrun_context_t *context, const char *id, oci_containe
           seccomp_flags_len = container->container_def->linux->seccomp->flags_len;
         }
 
+      exec_path = find_executable (process->args[0]);
+      if (UNLIKELY (exec_path == NULL))
+        {
+          if (errno == ENOENT)
+            crun_make_error (err, errno, "executable file not found in $PATH");
+          else
+            crun_make_error (err, errno, "open executable");
+
+          libcrun_fail_with_error ((*err)->status, "%s", (*err)->msg);
+        }
+
       if (!process->no_new_privileges)
         {
           ret = libcrun_apply_seccomp (seccomp_fd, seccomp_flags, seccomp_flags_len, err);
@@ -2012,17 +2022,6 @@ libcrun_container_exec (libcrun_context_t *context, const char *id, oci_containe
           ret = libcrun_set_caps (capabilities, container_uid, container_gid, process->no_new_privileges, err);
           if (UNLIKELY (ret < 0))
             libcrun_fail_with_error ((*err)->status, "%s", (*err)->msg);
-        }
-
-      exec_path = find_executable (process->args[0]);
-      if (UNLIKELY (exec_path == NULL))
-        {
-          if (errno == ENOENT)
-            crun_make_error (err, errno, "executable file not found in $PATH");
-          else
-            crun_make_error (err, errno, "open executable");
-
-          libcrun_fail_with_error ((*err)->status, "%s", (*err)->msg);
         }
 
       ret = close_fds_ge_than (context->preserve_fds + 3, err);

src/libcrun/utils.c
@@ -328,7 +328,7 @@ get_file_size (int fd, off_t *size)
 }
 
 static int
-get_file_type (mode_t *mode, const char *path, bool nofollow)
+get_file_type (mode_t *mode, const char *path)
 {
   struct stat st;
   int ret;
@@ -336,7 +336,7 @@ get_file_type (mode_t *mode, const char *path, bool nofollow)
 #ifdef HAVE_STATX
   struct statx stx;
 
-  ret = statx (AT_FDCWD, path, (nofollow ? AT_SYMLINK_NOFOLLOW : 0) | AT_STATX_DONT_SYNC, STATX_TYPE, &stx);
+  ret = statx (AT_FDCWD, path, AT_STATX_DONT_SYNC, STATX_TYPE, &stx);
   if (UNLIKELY (ret < 0))
     {
       if (errno == ENOSYS)
@@ -362,7 +362,7 @@ crun_dir_p (const char *path, libcrun_error_t *err)
   mode_t mode;
   int ret;
 
-  ret = get_file_type (&mode, path, true);
+  ret = get_file_type (&mode, path);
   if (UNLIKELY (ret < 0))
     return crun_make_error (err, errno, "error stat'ing file '%s'", path);
 
@@ -1014,7 +1014,7 @@ check_access (const char *path)
   if (ret < 0)
     return ret;
 
-  ret = get_file_type (&mode, path, false);
+  ret = get_file_type (&mode, path);
   if (UNLIKELY (ret < 0))
     return ret;
 