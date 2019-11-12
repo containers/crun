containers/crun

commit b9a0f0b75e7999ecc7e0d4df90fed24b1de5ac4a

utils: if the path exists check if it a directory
when creating a directory, if the path already exists make sure it is
a directory.x

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -604,10 +604,13 @@ create_dev (libcrun_container_t *container, int devfd, struct device_s *device, 
   const char *fullname = device->path;
   if (binds)
     {
-      cleanup_free char *path_to_container = NULL;
-      xasprintf (&path_to_container, "%s%s", rootfs, device->path);
+      char *path_to_container, buffer[PATH_MAX];
 
-      ret = crun_ensure_file (path_to_container, 0700, err);
+      path_to_container = chroot_realpath (rootfs, device->path, buffer);
+      if (path_to_container == NULL)
+        return crun_make_error (err, errno, "cannot resolve '%s'", device->path);
+
+      ret = crun_ensure_file (path_to_container, 0700, true, err);
       if (UNLIKELY (ret < 0))
         return ret;
 
@@ -645,7 +648,7 @@ create_dev (libcrun_container_t *container, int devfd, struct device_s *device, 
           if (tmp != resolved_path)
             {
               *tmp = '\0';
-              ret = crun_ensure_directory (resolved_path, 0700, err);
+              ret = crun_ensure_directory (resolved_path, 0700, true, err);
               if (UNLIKELY (ret < 0))
                 return ret;
               *tmp = '/';
@@ -756,7 +759,7 @@ do_masked_and_readonly_paths (libcrun_container_t *container, const char *rootfs
       if (ret == 0)
         continue;
 
-      dir = crun_dir_p (path, err);
+      dir = crun_dir_p (path, true, err);
       if (UNLIKELY (dir < 0))
         return ret;
 
@@ -945,7 +948,7 @@ do_mounts (libcrun_container_t *container, const char *rootfs, libcrun_error_t *
 
       if (def->mounts[i]->source && (flags & MS_BIND))
         {
-          is_dir = crun_dir_p (def->mounts[i]->source, err);
+          is_dir = crun_dir_p (def->mounts[i]->source, true, err);
           if (UNLIKELY (is_dir < 0))
             return is_dir;
 
@@ -970,13 +973,13 @@ do_mounts (libcrun_container_t *container, const char *rootfs, libcrun_error_t *
 
       if (is_dir)
         {
-          ret = crun_ensure_directory (target, 01755, err);
+          ret = crun_ensure_directory (target, 01755, true, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }
       else
         {
-          ret = crun_ensure_file (target, 0755, err);
+          ret = crun_ensure_file (target, 0755, true, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }
@@ -1124,7 +1127,7 @@ do_finalize_notify_socket (libcrun_container_t *container, libcrun_error_t *err)
   container_notify_socket_path_dir_alloc = xstrdup (container_notify_socket_path);
   container_notify_socket_path_dir = dirname (container_notify_socket_path_dir_alloc);
 
-  ret = crun_ensure_directory (container_notify_socket_path_dir, 0755, err);
+  ret = crun_ensure_directory (container_notify_socket_path_dir, 0755, false, err);
   if (UNLIKELY (ret < 0))
     return ret;
 

src/libcrun/cgroup.c
@@ -310,7 +310,7 @@ enter_cgroup_subsystem (pid_t pid, const char *subsystem, const char *path, int 
   xasprintf (&cgroup_path, "/sys/fs/cgroup/%s%s", subsystem, path ? path : "");
   if (ensure_missing)
     {
-      ret = crun_ensure_directory (cgroup_path, 0755, err);
+      ret = crun_ensure_directory (cgroup_path, 0755, false, err);
       if (UNLIKELY (ret < 0))
         {
           if (errno != EROFS)
@@ -461,7 +461,7 @@ enter_cgroup (int cgroup_mode, pid_t pid, const char *path, bool ensure_missing,
       xasprintf (&cgroup_path, "/sys/fs/cgroup/%s", path);
       if (ensure_missing)
         {
-          ret = crun_ensure_directory (cgroup_path, 0755, err);
+          ret = crun_ensure_directory (cgroup_path, 0755, false, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }

src/libcrun/status.c
@@ -47,7 +47,7 @@ get_run_directory (const char *state_root)
   if (root == NULL)
     root = xstrdup ("/run/crun");
 
-  ret = crun_ensure_directory (root, 0700, &err);
+  ret = crun_ensure_directory (root, 0700, false, &err);
   if (UNLIKELY (ret < 0))
     crun_error_release (&err);
   return root;
@@ -160,7 +160,7 @@ libcrun_status_check_directories (const char *state_root, const char *id, libcru
   cleanup_free char *run_directory = get_run_directory (state_root);
   int ret;
 
-  ret = crun_ensure_directory (run_directory, 0700, err);
+  ret = crun_ensure_directory (run_directory, 0700, false, err);
   if (UNLIKELY (ret < 0))
     return ret;
 

src/libcrun/utils.c
@@ -270,14 +270,26 @@ ensure_directory_internal (char *path, size_t len, int mode, libcrun_error_t *er
 }
 
 int
-crun_ensure_directory (const char *path, int mode, libcrun_error_t *err)
+crun_ensure_directory (const char *path, int mode, bool nofollow, libcrun_error_t *err)
 {
+  int ret;
   cleanup_free char *tmp = xstrdup (path);
-  return ensure_directory_internal (tmp, strlen (tmp), mode, err);
+  ret = ensure_directory_internal (tmp, strlen (tmp), mode, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  ret = crun_dir_p (path, nofollow, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  if (ret == 0)
+    return crun_make_error (err, ENOTDIR, "The path '%s' is not a directory", path);
+
+  return 0;
 }
 
 int
-crun_ensure_file (const char *path, int mode, libcrun_error_t *err)
+crun_ensure_file (const char *path, int mode, bool nofollow, libcrun_error_t *err)
 {
   cleanup_free char *tmp = xstrdup (path);
   size_t len = strlen (tmp);
@@ -289,7 +301,7 @@ crun_ensure_file (const char *path, int mode, libcrun_error_t *err)
   if (it > tmp)
     {
       *it = '\0';
-      ret = crun_ensure_directory (tmp, mode, err);
+      ret = crun_ensure_directory (tmp, mode, nofollow, err);
       if (UNLIKELY (ret < 0))
         return ret;
       *it = '/';
@@ -358,12 +370,12 @@ get_file_type (mode_t *mode, bool nofollow, const char *path)
 }
 
 int
-crun_dir_p (const char *path, libcrun_error_t *err)
+crun_dir_p (const char *path, bool nofollow, libcrun_error_t *err)
 {
   mode_t mode;
   int ret;
 
-  ret = get_file_type (&mode, true, path);
+  ret = get_file_type (&mode, nofollow, path);
   if (UNLIKELY (ret < 0))
     return crun_make_error (err, errno, "error stat'ing file '%s'", path);
 
@@ -1205,7 +1217,7 @@ check_access (const char *path)
   if (ret < 0)
     return ret;
 
-  ret = get_file_type (&mode, true, path);
+  ret = get_file_type (&mode, false, path);
   if (UNLIKELY (ret < 0))
     return ret;
 

src/libcrun/utils.h
@@ -69,11 +69,11 @@ int write_file (const char *name, const void *data, size_t len, libcrun_error_t 
 
 int write_file_at (int dirfd, const char *name, const void *data, size_t len, libcrun_error_t *err);
 
-int crun_ensure_directory (const char *path, int mode, libcrun_error_t *err);
+int crun_ensure_directory (const char *path, int mode, bool nofollow, libcrun_error_t *err);
 
-int crun_ensure_file (const char *path, int mode, libcrun_error_t *err);
+int crun_ensure_file (const char *path, int mode, bool nofollow, libcrun_error_t *err);
 
-int crun_dir_p (const char *path, libcrun_error_t *err);
+int crun_dir_p (const char *path, bool nofollow, libcrun_error_t *err);
 
 int detach_process ();
 

tests/tests_libcrun_utils.c
@@ -173,11 +173,11 @@ static int
 test_dir_p ()
 {
   libcrun_error_t err;
-  if (crun_dir_p ("/usr", &err) <= 0)
+  if (crun_dir_p ("/usr", false, &err) <= 0)
     return -1;
-  if (crun_dir_p ("/dev/zero", &err) != 0)
+  if (crun_dir_p ("/dev/zero", false, &err) != 0)
     return -1;
-  if (crun_dir_p ("/hopefully/does/not/really/exist", &err) >= 0)
+  if (crun_dir_p ("/hopefully/does/not/really/exist", false, &err) >= 0)
     return -1;
   return 0;
 }

commit 464e3d38072cba935cba582f7a6945c5a45db5e3

linux: resolve source paths
make sure the the source path is resolved when checking the file
type.

regression introduced with b9a0f0b75e7999ecc7e0d4df90fed24b1de5ac4a

Closes: https://github.com/containers/toolbox/issues/330

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -958,7 +958,7 @@ do_mounts (libcrun_container_t *container, const char *rootfs, libcrun_error_t *
 
       if (def->mounts[i]->source && (flags & MS_BIND))
         {
-          is_dir = crun_dir_p (def->mounts[i]->source, true, err);
+          is_dir = crun_dir_p (def->mounts[i]->source, false, err);
           if (UNLIKELY (is_dir < 0))
             return is_dir;
 