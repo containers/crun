1eb8a65e08012e71bc44cf30036a1b8d96389739
Giuseppe Scrivano <giuseppe@scrivano.org>
2019-12-10 23:12:28 +0100

    travis: add containerd tests
    Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

---
+++ tests/containerd/run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $1
+
+if test "$(id -u)" != 0; then
+	echo "run as root"
+	exit 1
+fi
+
+(cd /crun && git clean -fdx && ./autogen.sh && \
+     ./configure CFLAGS='-Wall -Wextra -Werror' && make -j $(nproc) && cp crun /usr/bin/runc)
+
+ulimit -u unlimited
+
+export PATH=$PATH:$(pwd)/bin
+
+make TEST_RUNTIME=io.containerd.runc.v2 integration
