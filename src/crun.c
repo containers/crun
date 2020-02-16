17f7f9e2dccdf557a94783a237f931b3bbd2c9ef
Giuseppe Scrivano <gscrivan@redhat.com>
2020-02-16 13:01:11 +0100

    crun: add CRIU to the features list
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- src/crun.c
+++ src/crun.c
@@ -194,6 +194,9 @@ print_version (FILE *stream, struct argp_state *state arg_unused)
 #ifdef HAVE_EBPF
   fprintf (stream, "+EBPF ");
 #endif
+#ifdef HAVE_CRIU
+  fprintf (stream, "+CRIU ");
+#endif
   fprintf (stream, "+YAJL\n");
 }
 
