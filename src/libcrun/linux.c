containers/crun

commit 2c681f8ef8670a927132faaa5585fb1e1fc122df

libcrun: always use setsid
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -2916,12 +2916,6 @@ libcrun_join_process (libcrun_container_t *container, pid_t pid_to_join, libcrun
   for (i = 0; all_namespaces[i]; i++)
     close_and_reset (&fds[i]);
 
-  if (detach && setsid () < 0)
-    {
-      crun_make_error (err, errno, "setsid");
-      goto exit;
-    }
-
   /* We need to fork once again to join the PID namespace.  */
   pid = fork ();
   if (UNLIKELY (pid < 0))

src/libcrun/container.c
@@ -680,13 +680,13 @@ container_init_setup (void *args, const char *notify_socket,
         }
     }
 
+  ret = setsid ();
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "setsid");
+
   if (has_terminal)
     {
       cleanup_close int terminal_fd = -1;
-
-      ret = setsid ();
-      if (UNLIKELY (ret < 0))
-        return crun_make_error (err, errno, "setsid");
 
       fflush (stderr);
 

commit 4f5490f208afb05a1913c5274d10b079a4803f2f

linux: exec reintroduce setsid on detach
regression introduced by 2c681f8ef8670a927132faaa5585fb1e1fc122df

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -2917,6 +2917,12 @@ libcrun_join_process (libcrun_container_t *container, pid_t pid_to_join, libcrun
   for (i = 0; all_namespaces[i]; i++)
     close_and_reset (&fds[i]);
 
+  if (setsid () < 0)
+    {
+      crun_make_error (err, errno, "setsid");
+      goto exit;
+    }
+
   /* We need to fork once again to join the PID namespace.  */
   pid = fork ();
   if (UNLIKELY (pid < 0))