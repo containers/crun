34a0ff3cc9fc588bf27d959987b1ecbea6261e15
Giuseppe Scrivano <gscrivan@redhat.com>
2020-02-25 22:05:09 +0100

    license: change license to gplv2+ and lgpl2.1+
    change the license to GPLv2+ and for libcrun to LGPLv2.1+.
    
    Closes: https://github.com/containers/crun/issues/256
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- cfg.mk
+++ cfg.mk
@@ -12,7 +12,8 @@ local-checks-to-skip = \
     sc_unmarked_diagnostics \
     sc_prohibit_always_true_header_tests \
     sc_prohibit_intprops_without_use \
-    sc_error_message_uppercase
+    sc_error_message_uppercase \
+    sc_GPL_version
 
 
 #SHELL=bash -x
