containers/crun

commit 8e580bcb332a3dea7ef910ee297d1f595540b869

utils: add functions to safe create directories/files
add crun_safe_ensure_file_at and crun_safe_ensure_directory_at.
They are slower than the crun_ensure_file/directory_at variant but
they detect if any path is changed during the lookup and prevent
files/directories to be created outside of the rootfs.

Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

src/libcrun/utils.c
@@ -289,7 +289,7 @@ create_file_if_missing_at (int dirfd, const char *file, libcrun_error_t *err)
 int
 create_file_if_missing (const char *file, libcrun_error_t *err)
 {
-  cleanup_close int fd_write = open (file, O_CREAT | O_WRONLY, 0700);
+  cleanup_close int fd_write = open (file, O_CLOEXEC | O_CREAT | O_WRONLY, 0700);
   if (fd_write < 0)
     {
       mode_t mode;
@@ -373,6 +373,110 @@ crun_ensure_directory_at (int dirfd, const char *path, int mode, bool nofollow, 
     return crun_make_error (err, ENOTDIR, "The path `%s` is not a directory", path);
 
   return 0;
+}
+
+int
+check_fd_under_path (const char *rootfs, size_t rootfslen, int fd, const char *fdname, libcrun_error_t *err)
+{
+  int ret;
+  char link[PATH_MAX];
+  char fdpath[64];
+
+  sprintf (fdpath, "/proc/self/fd/%d", fd);
+  ret = TEMP_FAILURE_RETRY (readlink (fdpath, link, sizeof (link)));
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "readlink `%s`", fdname);
+
+  if (((size_t) ret) <= rootfslen || memcmp (link, rootfs, rootfslen) != 0)
+    return crun_make_error (err, 0, "target `%s` not under the directory `%s`", fdname, rootfs);
+
+  return 0;
+}
+
+static int
+crun_safe_ensure_at (bool dir, int dirfd, const char *dirpath, size_t dirpath_len, const char *path,
+                     int mode, libcrun_error_t *err)
+{
+  cleanup_free char *npath = xstrdup (path);
+  cleanup_close int wd_file_cleanup = -1;
+  cleanup_close int wd_cleanup = -1;
+  bool last_component = false;
+  const char *cur;
+  char *it;
+  int cwd;
+  int ret;
+
+  it = npath + strlen (npath) - 1;
+  while (*it == '/' && it > npath && ((size_t) (it - path)) > dirpath_len)
+    *it-- = '\0';
+  if (((size_t) (it - path)) == dirpath_len)
+    return crun_make_error (err, 0, "invalid path `%s`", path);
+
+  cwd = dirfd;
+  cur = npath;
+  it = strchr (npath, '/');
+  while (cur && cur[0])
+    {
+      if (it)
+        *it = '\0';
+      else
+        last_component = true;
+
+      if (!last_component || dir)
+        ret = mkdirat (cwd, cur, mode);
+      else
+        {
+          ret = wd_file_cleanup = openat (cwd, cur, O_CLOEXEC | O_CREAT | O_WRONLY, 0700);
+          if (UNLIKELY (ret < 0))
+            return crun_make_error (err, errno, "create `%s`", path);
+
+          ret = check_fd_under_path (dirpath, dirpath_len, wd_file_cleanup, path, err);
+          if (UNLIKELY (ret < 0))
+            return ret;
+        }
+
+      if (ret < 0)
+        {
+          if (errno != EEXIST)
+            return crun_make_error (err, errno, "mkdir `%s`", cur);
+        }
+
+      cwd = openat (cwd, cur, O_CLOEXEC | O_PATH);
+      if (wd_cleanup >= 0)
+        {
+          TEMP_FAILURE_RETRY (close (wd_cleanup));
+          wd_cleanup = -1;
+        }
+      wd_cleanup = cwd;
+      if (UNLIKELY (cwd < 0))
+        return crun_make_error (err, errno, "open `%s`", cur);
+
+      ret = check_fd_under_path (dirpath, dirpath_len, cwd, cur, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+
+      if (it == NULL)
+        break;
+
+      cur = it + 1;
+      it = strchr (cur, '/');
+    }
+
+  return 0;
+}
+
+int
+crun_safe_ensure_directory_at (int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode,
+                               libcrun_error_t *err)
+{
+  return crun_safe_ensure_at (true, dirfd, dirpath, dirpath_len, path, mode, err);
+}
+
+int
+crun_safe_ensure_file_at (int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode,
+                          libcrun_error_t *err)
+{
+  return crun_safe_ensure_at (false, dirfd, dirpath, dirpath_len, path, mode, err);
 }
 
 int
@@ -683,7 +787,7 @@ open_unix_domain_client_socket (const char *path, int dgram, libcrun_error_t *er
 
   memset (&addr, 0, sizeof (addr));
   if (strlen (path) >= sizeof (addr.sun_path))
-    return crun_make_error (err, errno, "invalid path %s specified", path);
+    return crun_make_error (err, 0, "invalid path %s specified", path);
   strcpy (addr.sun_path, path);
   addr.sun_family = AF_UNIX;
   ret = connect (fd, (struct sockaddr *) &addr, sizeof (addr));
@@ -707,7 +811,7 @@ open_unix_domain_socket (const char *path, int dgram, libcrun_error_t *err)
 
   memset (&addr, 0, sizeof (addr));
   if (strlen (path) >= sizeof (addr.sun_path))
-    return crun_make_error (err, errno, "invalid path %s specified", path);
+    return crun_make_error (err, 0, "invalid path %s specified", path);
   strcpy (addr.sun_path, path);
   addr.sun_family = AF_UNIX;
   ret = bind (fd, (struct sockaddr *) &addr, sizeof (addr));

src/libcrun/linux.c
@@ -362,21 +362,10 @@ finalize_mounts (libcrun_container_t *container, libcrun_error_t *err)
 static int
 check_fd_under_rootfs (libcrun_container_t *container, int fd, const char *target, libcrun_error_t *err)
 {
-  int ret;
-  char link[PATH_MAX];
-  char fdpath[64];
   const char *rootfs = get_private_data (container)->rootfs;
   size_t rootfslen = get_private_data (container)->rootfslen;
 
-  sprintf (fdpath, "/proc/self/fd/%d", fd);
-  ret = TEMP_FAILURE_RETRY (readlink (fdpath, link, sizeof (link)));
-  if (UNLIKELY (ret < 0))
-    return crun_make_error (err, errno, "readlink `%s`", target);
-
-  if (ret <= rootfslen || memcmp (link, rootfs, rootfslen) != 0)
-    return crun_make_error (err, 0, "target `%s` not under the rootfs", target);
-
-  return 0;
+  return check_fd_under_path (rootfs, rootfslen, fd, target, err);
 }
 
 static int
@@ -1204,13 +1193,13 @@ do_mounts (libcrun_container_t *container, int rootfsfd, const char *rootfs, lib
 
       if (is_dir)
         {
-          ret = crun_ensure_directory_at (rootfsfd, target_rel, 01755, true, err);
+          ret = crun_safe_ensure_directory_at (rootfsfd, rootfs, rootfslen, target_rel, 01755, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }
       else
         {
-          ret = crun_ensure_file_at (rootfsfd, target_rel, 0755, true, err);
+          ret = crun_safe_ensure_file_at (rootfsfd, rootfs, rootfslen, target_rel, 0755, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }

src/libcrun/utils.h
@@ -78,6 +78,12 @@ int crun_ensure_directory_at (int dirfd, const char *path, int mode, bool nofoll
 
 int crun_ensure_file_at (int dirfd, const char *path, int mode, bool nofollow, libcrun_error_t *err);
 
+int check_fd_under_path (const char *rootfs, size_t rootfslen, int fd, const char *fdname, libcrun_error_t *err);
+
+int crun_safe_ensure_directory_at (int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode, libcrun_error_t *err);
+
+int crun_safe_ensure_file_at (int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode, libcrun_error_t *err);
+
 int crun_dir_p (const char *path, bool nofollow, libcrun_error_t *err);
 
 int crun_dir_p_at (int dirfd, const char *path, bool nofollow, libcrun_error_t *err);

commit 7b231fde79cad4e0646b48a982094b4f68c543c0

utils: fix mount on not writeable path
Fix checking if a file is under the rootfs and allow to bind mount on
not writeable paths.  If the open(O_CREAT) fails, then attempt to open
the file in O_PATH mode.

commit 8e580bcb332a3dea7ef910ee297d1f595540b869 introduced the
regression.

Closes: https://github.com/containers/crun/issues/290

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/utils.c
@@ -441,15 +441,18 @@ crun_safe_ensure_at (bool dir, int dirfd, const char *dirpath, size_t dirpath_le
         {
           ret = openat (cwd, cur, O_CLOEXEC | O_CREAT | O_WRONLY, 0700);
           if (UNLIKELY (ret < 0))
-            return crun_make_error (err, errno, "create `%s`", path);
+            {
+              int saved_errno = errno;
+
+              /* If the previous openat fails, attempt to open the file in O_PATH mode.  */
+              ret = openat (cwd, cur, O_CLOEXEC | O_PATH);
+              if (ret < 0)
+                return crun_make_error (err, saved_errno, "create `%s`", path);
+            }
 
           close_and_replace (&wd_cleanup, ret);
 
-          ret = check_fd_under_path (dirpath, dirpath_len, ret, path, err);
-          if (UNLIKELY (ret < 0))
-            return ret;
-
-          return 0;
+          return check_fd_under_path (dirpath, dirpath_len, ret, path, err);
         }
 
       if (ret < 0)