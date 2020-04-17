containers/crun

commit eaccb4b2ad035988962f04f4ac2c5c41568424bf

cgroup: first implementation
Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

src/libcrun/cgroup.c
@@ -1,0 +1,286 @@
+/*
+ * crun - OCI runtime written in C
+ *
+ * Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
+ * libocispec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libocispec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with crun.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <config.h>
+#include "cgroup.h"
+#include "utils.h"
+#include <string.h>
+#include <sys/types.h>
+#include <systemd/sd-bus.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+static int
+enter_cgroup (pid_t pid, const char *path, libcrun_error_t *err)
+{
+  cleanup_free char *cgroup_path;
+  cleanup_free char *cgroup_path_procs;
+  cleanup_close int fd = -1;
+  char pid_str[16];
+  int ret;
+
+  xasprintf (&cgroup_path, "/sys/fs/cgroup/unified/%s", path);
+
+  ret = mkdir (cgroup_path, 0755);
+  if (UNLIKELY (ret < 0 && errno != EEXIST))
+    return crun_make_error (err, errno, "creating cgroup '%s'", path);
+
+  xasprintf (&cgroup_path_procs, "/sys/fs/cgroup/unified/%s/cgroup.procs", path);
+
+  fd = open (cgroup_path_procs, O_WRONLY);
+  if (UNLIKELY (fd < 0))
+    return crun_make_error (err, errno, "opening '%s'", cgroup_path_procs);
+
+  sprintf (pid_str, "%d", pid);
+
+  do
+    ret = write (fd, pid_str, strlen (pid_str));
+  while (ret < 0 && errno == EINTR);
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "cannot write to '%s'", cgroup_path_procs);
+
+  return 0;
+}
+
+static
+int get_current_path (char **path, const char *suffix, libcrun_error_t *err)
+{
+  cleanup_free char *content = NULL;
+  int ret;
+  char *from, *to;
+
+  ret = read_all_file ("/proc/self/cgroup", &content, NULL, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+  from = strstr (content, "0::");
+  if (UNLIKELY (from == NULL))
+    return crun_make_error (err, 0, "cannot find cgroup2 for the current process");
+
+  from += 3;
+  to = strchr (from, '\n');
+  if (UNLIKELY (to == NULL))
+    return crun_make_error (err, 0, "cannot parse /proc/self/cgroup");
+  *to = '\0';
+
+  if (suffix)
+    xasprintf (path, "%s/%s", from, suffix);
+  else
+    *path = xstrdup (from);
+
+  return 0;
+}
+
+struct systemd_job_removed_s
+{
+  const char *path;
+  int *terminated;
+};
+
+static int
+systemd_job_removed (sd_bus_message *m, void *userdata, sd_bus_error *error)
+{
+  const char *path, *unit, *result;
+  uint32_t id;
+  int ret;
+  struct systemd_job_removed_s *p = userdata;
+  
+  ret = sd_bus_message_read (m, "uoss", &id, &path, &unit, &result);
+  if (ret < 0)
+    return -1;
+
+  if (strcmp (p->path, path) == 0)
+    *p->terminated = 1;
+  return 0;
+}
+
+static
+int enter_system_cgroup_scope (char **path, const char *scope, pid_t pid, libcrun_error_t *err)
+{
+  sd_bus *bus = NULL;
+  sd_bus_message *m = NULL;
+  sd_bus_message *reply = NULL;
+  int ret = 0;
+  sd_bus_error error = SD_BUS_ERROR_NULL;
+  const char *object;
+  int terminated = 0;
+  struct systemd_job_removed_s userdata;
+
+  if (sd_bus_default (&bus) < 0)
+    {
+      ret = crun_make_error (err, 0, "cannot open sd-bus");
+      goto exit;
+    }
+
+  ret = sd_bus_add_match (bus,
+                          NULL,
+                          "type='signal',"
+                          "sender='org.freedesktop.systemd1',"
+                          "interface='org.freedesktop.systemd1.Manager',"
+                          "member='JobRemoved',"
+                          "path='/org/freedesktop/systemd1'",
+                          systemd_job_removed, &userdata);
+  if (UNLIKELY (ret < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus message read");
+      goto exit;
+    }
+
+  if (UNLIKELY (sd_bus_message_new_method_call (bus, &m,
+                                                "org.freedesktop.systemd1",
+                                                "/org/freedesktop/systemd1",
+                                                "org.freedesktop.systemd1.Manager",
+                                                "StartTransientUnit") < 0))
+    {
+      ret = crun_make_error (err, 0, "set up dbus message");
+      goto exit;
+    }
+  if (UNLIKELY (sd_bus_message_append (m, "ss", scope, "fail") < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus message append");
+      goto exit;
+    }
+  
+  if (UNLIKELY (sd_bus_message_open_container (m, 'a', "(sv)") < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus open container");
+      goto exit;
+    }
+
+  if (UNLIKELY (sd_bus_message_append (m, "(sv)", "Description", "s", "libcrun container") < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus message append");
+      goto exit;
+    }
+
+  if (UNLIKELY (sd_bus_message_append (m, "(sv)", "PIDs", "au", 1, pid) < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus message append");
+      goto exit;
+    }
+
+  if (UNLIKELY (sd_bus_message_close_container (m) < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus close container");
+      goto exit;
+    }
+
+  if (UNLIKELY (sd_bus_message_append (m, "a(sa(sv))", 0) < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus message append");
+      goto exit;
+    }
+
+  ret = sd_bus_call (bus, m, 0, &error, &reply);
+  if (UNLIKELY (ret < 0))
+    {
+      ret = crun_make_error (err, sd_bus_error_get_errno (&error), "sd-bus call");
+      goto exit;
+    }
+
+  ret = sd_bus_message_read (reply, "o", &object);
+  if (UNLIKELY (ret < 0))
+    {
+      ret = crun_make_error (err, 0, "sd-bus message read");
+      goto exit;
+    }
+
+  userdata.path = object;
+  userdata.terminated = &terminated;
+  while (!terminated)
+    {
+      ret = sd_bus_process (bus, NULL);
+      if (UNLIKELY (ret < 0))
+        {
+          ret = crun_make_error (err, 0, "sd-bus process");
+          break;
+        }
+
+      if (ret == 0)
+        {
+          ret = sd_bus_wait (bus, (uint64_t) -1);
+          if (UNLIKELY (ret < 0))
+            {
+              ret = crun_make_error (err, 0, "sd-bus wait");
+              break;
+            }
+          continue;
+        }
+
+      if (UNLIKELY (ret < 0))
+        {
+          ret = crun_make_error (err, 0, "sd-bus wait");
+          break;
+        }
+    }
+  
+exit:
+  if (bus)
+    sd_bus_unref (bus);
+  if (m)
+    sd_bus_message_unref (m);
+  if (reply)
+    sd_bus_message_unref (reply);
+  sd_bus_error_free (&error);
+  return ret;
+}
+
+int
+libcrun_cgroup_enter (char **path, int systemd, pid_t pid, const char *id, libcrun_error_t *err)
+{
+  int ret;
+  cleanup_free char *scope;
+  xasprintf (&scope, "%s.scope", id);
+
+  if (systemd)
+    {
+      ret = enter_system_cgroup_scope (path, scope, pid, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+      return get_current_path (path, NULL, err);
+    }
+  else
+    {
+      ret = get_current_path (path, scope, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+      return enter_cgroup (pid, *path, err);
+    }
+  return 0;
+}
+
+
+int
+libcrun_cgroup_killall (char *path, libcrun_error_t *err)
+{
+  return 0;
+}
+
+int
+libcrun_cgroup_destroy (char *path, libcrun_error_t *err)
+{
+  cleanup_free char *cgroup_path;
+  int ret;
+
+  xasprintf (&cgroup_path, "/sys/fs/cgroup/unified/%s", path);
+
+  ret = rmdir (cgroup_path);
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "deleting cgroup '%s'", path);
+
+  return 0;
+}

Makefile.am
@@ -6,6 +6,7 @@ bin_PROGRAMS = crun
 
 libcrun_a_SOURCES = src/libcrun/utils.c \
 		src/libcrun/container.c \
+		src/libcrun/cgroup.c \
 		src/libcrun/linux.c \
 		src/libcrun/seccomp.c \
 		src/libcrun/error.c \
@@ -19,5 +20,5 @@ crun_SOURCES = src/crun.c src/run.c src/delete.c src/kill.c
 crun_LDADD = libcrun.a libocispec/libocispec.a $(SELINUX_LIBS) $(YAJL_LIBS)
 
 EXTRA_DIST = src/crun.h src/run.h src/seccomp.h src/delete.h src/kill.h \
-	src/libcrun/container.h src/libcrun/linux.h src/libcrun/utils.h \
-	src/libcrun/error.h src/libcrun/status.h
+	src/libcrun/container.h src/libcrun/cgroup.h src/libcrun/linux.h \
+	src/libcrun/utils.h src/libcrun/error.h src/libcrun/status.h

configure.ac
@@ -18,6 +18,7 @@ AC_CHECK_HEADERS([sys/capability.h], [], [AC_MSG_ERROR([*** POSIX caps headers n
 
 AC_CHECK_LIB(cap, cap_from_name)
 AC_CHECK_LIB(seccomp, seccomp_rule_add)
+AC_CHECK_LIB(systemd, sd_bus_open)
 
 PKG_CHECK_MODULES([YAJL], [yajl >= 2.0.0])
 PKG_CHECK_MODULES([SELINUX], [libselinux >= 2.1.9], [AC_DEFINE(HAVE_SELINUX, 1, [Define if SELinux is available])], [])

src/libcrun/cgroup.h
@@ -15,25 +15,14 @@
  * You should have received a copy of the GNU General Public License
  * along with crun.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef ERROR_H
-# define ERROR_H
-# include <config.h>
-# include <error.h>
-# include <stdlib.h>
+#ifndef CGROUP_H
+# define CGROUP_H
 
-struct libcrun_error_s
-{
-  int status;
-  char *msg;
-};
-typedef struct libcrun_error_s *libcrun_error_t;
+# include "container.h"
+# include <unistd.h>
 
-void oom_handler ();
-
-# define OOM() do {oom_handler ();} while (0)
-
-int crun_make_error (libcrun_error_t *err, int status, const char *msg, ...);
-
-int crun_error_release (libcrun_error_t *err);
+int libcrun_cgroup_enter (char **path, int systemd, pid_t pid, const char *id, libcrun_error_t *err);
+int libcrun_cgroup_killall (char *path, libcrun_error_t *err);
+int libcrun_cgroup_destroy (char *path, libcrun_error_t *err);
 
 #endif

src/libcrun/error.c
@@ -57,3 +57,9 @@ crun_error_release (libcrun_error_t *err)
   *err = NULL;
   return 0;
 }
+
+void
+oom_handler ()
+{
+  error (EXIT_FAILURE, 0, "OOM");
+}

src/libcrun/utils.c
@@ -54,6 +54,16 @@ xmalloc (size_t size)
   return res;
 }
 
+void *
+xrealloc (void *ptr, size_t size)
+{
+  void *res = realloc (ptr, size);
+  if (UNLIKELY (res == NULL))
+    OOM ();
+  return res;
+}
+
+
 char *
 argp_mandatory_argument (char *arg, struct argp_state *state)
 {
@@ -233,7 +243,7 @@ read_all_file (const char *path, char **out, size_t *len, libcrun_error_t *err)
   int ret;
   cleanup_free char *buf = NULL;
   struct stat stat;
-  size_t nread, left;
+  size_t nread, allocated;
 
   do
     fd = open (path, O_RDONLY);
@@ -247,26 +257,28 @@ read_all_file (const char *path, char **out, size_t *len, libcrun_error_t *err)
     return crun_make_error (err, 0, "error stat'ing file '%s'", path);
 
   /* NUL terminate the buffer.  */
-  buf = xmalloc (stat.st_size + 1);
+  allocated = stat.st_size;
+  if (stat.st_size)
+    allocated = 256;
+  buf = xmalloc (allocated + 1);
   buf[stat.st_size] = '\0';
   nread = 0;
-  left = stat.st_size;
-  while (left)
+  while ((stat.st_size && nread < stat.st_size) || 1)
     {
       do
-        ret = read (fd, buf + nread, left);
+        ret = read (fd, buf + nread, allocated - nread);
       while (ret < 0 && errno == EINTR);
       if (UNLIKELY (ret < 0))
         return crun_make_error (err, 0, "error reading from file '%s'", path);
 
-      left -= ret;
       nread += ret;
 
-      /* File probably changed its size, so return earlier.
-         We don't handle the case when the file grows between the stat
-         and the read here.  */
-      if (ret < left)
+      if (nread < allocated)
         break;
+
+      allocated += 256;
+      buf = xrealloc (buf, allocated + 1);
+      buf[allocated] = '\0';
     }
   *out = buf;
   buf = NULL;

src/libcrun/utils.h
@@ -36,6 +36,8 @@ void cleanup_closep (void *p);
 
 void *xmalloc (size_t size);
 
+void *xrealloc (void *ptr, size_t size);
+
 char *xstrdup (const char *str);
 
 int xasprintf (char **str, const char *fmt, ...);

commit 2a671667d232d0559d291f527ece8d96fa5ee2b3

cgroup: check systemd unit creation/removal status
While playing with Fedora 31 host with old/broken selinux packages,
I found out that systemd fails to create a transient unit. Here is
an except from journalctl:

> audit[1]: AVC avc:  denied  { setsched } for  pid=1 comm="systemd" scontext=system_u:system_r:init_t:s0 tcontext=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023 tclass=process permissive=0
> systemd[1]: crun-555.scope: Failed to add PIDs to scope's control group: Permission denied
> systemd[1]: crun-555.scope: Failed with result 'resources'.
> systemd[1]: Failed to start libcrun container.

and yet crun did not show any error and proceeded to start the
container, which lead to a number of issues.

1. Since the cgroup was not created by systemd, but the error
was not detected, the container process was not put into its own
cgroup (but left in the same cgroup as the shell from which `crun`
was called).

2. Since crun gets the cgroup name from /proc/$PID/cgroup
(where $PID is container process PID), it proceeded to set the
limits for that (wrong) cgroup:

 # cat /sys/fs/cgroup/system.slice/sshd.service/memory.max
 536870912

3. `crun delete` apparently removes the `system.slice/sshd.service`
cgroup :(

The primary cause is the missing check that the transient unit has
been created. This is what this patch adds (similar to how it's done
in cgroup-run code).

After this patch:

 # ../crun --systemd-cgroup run -d 555
 2020-04-16T14:47:34.000354150Z: error creating systemd unit crun-555.scope: failed

For more background on how the issue was found, steps to repro etc
please see a similar (but much less brutal -- it just fails to start
the container) issue in runc:

 - https://github.com/opencontainers/runc/issues/2313

While at it, abstract out the code preparing and doing the check.

Fixes: eaccb4b2ad0359
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

src/libcrun/cgroup.c
@@ -720,7 +720,9 @@ int systemd_finalize (struct libcrun_cgroup_args *args, const char *suffix, libc
 struct systemd_job_removed_s
 {
   const char *path;
-  int *terminated;
+  const char *op;
+  int terminated;
+  libcrun_error_t err;
 };
 
 static int
@@ -729,14 +731,74 @@ systemd_job_removed (sd_bus_message *m, void *userdata, sd_bus_error *error arg_
   const char *path, *unit, *result;
   uint32_t id;
   int ret;
-  struct systemd_job_removed_s *p = userdata;
+  struct systemd_job_removed_s *d = userdata;
 
   ret = sd_bus_message_read (m, "uoss", &id, &path, &unit, &result);
   if (ret < 0)
     return -1;
 
-  if (strcmp (p->path, path) == 0)
-    *p->terminated = 1;
+  if (strcmp (d->path, path) == 0)
+    {
+      d->terminated = 1;
+      if (strcmp (result, "done") != 0)
+        crun_make_error (&d->err, 0, "error %s systemd unit `%s`: got `%s`", d->op, unit, result);
+    }
+  return 0;
+}
+
+static int
+systemd_check_job_status_setup (sd_bus *bus,
+                                struct systemd_job_removed_s *data,
+                                libcrun_error_t *err)
+{
+  int ret;
+
+  ret = sd_bus_match_signal_async (bus,
+                                   NULL,
+                                   "org.freedesktop.systemd1",
+                                   "/org/freedesktop/systemd1",
+                                   "org.freedesktop.systemd1.Manager",
+                                   "JobRemoved",
+                                   systemd_job_removed,
+                                   NULL,
+                                   data);
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, -ret, "sd-bus match signal");
+
+  return 0;
+}
+
+static int
+systemd_check_job_status (sd_bus *bus,
+                          struct systemd_job_removed_s *data,
+                          const char *path,
+                          const char *op,
+                          libcrun_error_t *err)
+{
+  int sd_err;
+
+  data->path = path;
+  data->op = op;
+  while (!data->terminated)
+    {
+      sd_err = sd_bus_process (bus, NULL);
+      if (UNLIKELY (sd_err < 0))
+        return crun_make_error (err, -sd_err, "sd-bus process");
+
+      if (sd_err != 0)
+        continue;
+
+      sd_err = sd_bus_wait (bus, (uint64_t) -1);
+      if (UNLIKELY (sd_err < 0))
+        return crun_make_error (err, -sd_err, "sd-bus wait");
+    }
+
+  if (data->err != NULL)
+    {
+      *err = data->err;
+      return -1;
+    }
+
   return 0;
 }
 
@@ -890,8 +952,7 @@ int enter_systemd_cgroup_scope (runtime_spec_schema_config_linux_resources *reso
   int sd_err, ret = 0;
   sd_bus_error error = SD_BUS_ERROR_NULL;
   const char *object;
-  int terminated = 0;
-  struct systemd_job_removed_s userdata;
+  struct systemd_job_removed_s job_data = {};
   int i;
   const char *boolean_opts[10];
 
@@ -922,19 +983,9 @@ int enter_systemd_cgroup_scope (runtime_spec_schema_config_linux_resources *reso
         }
     }
 
-  sd_err = sd_bus_add_match (bus,
-                          NULL,
-                          "type='signal',"
-                          "sender='org.freedesktop.systemd1',"
-                          "interface='org.freedesktop.systemd1.Manager',"
-                          "member='JobRemoved',"
-                          "path='/org/freedesktop/systemd1'",
-                          systemd_job_removed, &userdata);
-  if (UNLIKELY (sd_err < 0))
-    {
-      ret = crun_make_error (err, -sd_err, "sd-bus add match");
+  ret = systemd_check_job_status_setup (bus, &job_data, err);
+  if (UNLIKELY (ret < 0))
       goto exit;
-    }
 
   sd_err = sd_bus_message_new_method_call (bus, &m, "org.freedesktop.systemd1",
                                            "/org/freedesktop/systemd1",
@@ -1050,28 +1101,7 @@ int enter_systemd_cgroup_scope (runtime_spec_schema_config_linux_resources *reso
       goto exit;
     }
 
-  userdata.path = object;
-  userdata.terminated = &terminated;
-  while (!terminated)
-    {
-      sd_err = sd_bus_process (bus, NULL);
-      if (UNLIKELY (sd_err < 0))
-        {
-          ret = crun_make_error (err, -sd_err, "sd-bus process");
-          break;
-        }
-
-      if (sd_err == 0)
-        {
-          sd_err = sd_bus_wait (bus, (uint64_t) -1);
-          if (UNLIKELY (sd_err < 0))
-            {
-              ret = crun_make_error (err, -sd_err, "sd-bus wait");
-              break;
-            }
-          continue;
-        }
-    }
+  ret = systemd_check_job_status (bus, &job_data, object, "creating", err);
 
 exit:
   if (bus)
@@ -1093,8 +1123,7 @@ int destroy_systemd_cgroup_scope (const char *scope, libcrun_error_t *err)
   int ret = 0;
   sd_bus_error error = SD_BUS_ERROR_NULL;
   const char *object;
-  int terminated = 0;
-  struct systemd_job_removed_s userdata;
+  struct systemd_job_removed_s job_data = {};
 
   if (sd_bus_default (&bus) < 0)
     {
@@ -1102,19 +1131,9 @@ int destroy_systemd_cgroup_scope (const char *scope, libcrun_error_t *err)
       goto exit;
     }
 
-  ret = sd_bus_add_match (bus,
-                          NULL,
-                          "type='signal',"
-                          "sender='org.freedesktop.systemd1',"
-                          "interface='org.freedesktop.systemd1.Manager',"
-                          "member='JobRemoved',"
-                          "path='/org/freedesktop/systemd1'",
-                          systemd_job_removed, &userdata);
+  ret = systemd_check_job_status_setup (bus, &job_data, err);
   if (UNLIKELY (ret < 0))
-    {
-      ret = crun_make_error (err, -ret, "sd-bus message read");
-      goto exit;
-    }
+    goto exit;
 
   ret = sd_bus_message_new_method_call (bus, &m,
                                         "org.freedesktop.systemd1",
@@ -1147,28 +1166,8 @@ int destroy_systemd_cgroup_scope (const char *scope, libcrun_error_t *err)
       goto exit;
     }
 
-  userdata.path = object;
-  userdata.terminated = &terminated;
-  while (!terminated)
-    {
-      ret = sd_bus_process (bus, NULL);
-      if (UNLIKELY (ret < 0))
-        {
-          ret = crun_make_error (err, -ret, "sd-bus process");
-          break;
-        }
+  ret = systemd_check_job_status (bus, &job_data, object, "removing", err);
 
-      if (ret == 0)
-        {
-          ret = sd_bus_wait (bus, (uint64_t) -1);
-          if (UNLIKELY (ret < 0))
-            {
-              ret = crun_make_error (err, -ret, "sd-bus wait");
-              break;
-            }
-          continue;
-        }
-    }
 exit:
   if (bus)
     sd_bus_unref (bus);