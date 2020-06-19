containers/crun

commit 7179d71afd608f5e85740b2b4b83f9b41fbe1b15

linux: always cleanup status on startup errors
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/container.c
@@ -1374,11 +1374,9 @@ flush_fd_to_err (libcrun_context_t *context, int terminal_fd)
 }
 
 static int
-cleanup_watch (libcrun_context_t *context, pid_t init_pid, runtime_spec_schema_config_schema *def, const char *id, int sync_socket, int terminal_fd, libcrun_error_t *err)
+cleanup_watch (libcrun_context_t *context, pid_t init_pid, int sync_socket, int terminal_fd, libcrun_error_t *err)
 {
   libcrun_error_t tmp_err = NULL;
-  container_delete_internal (context, def, id, 1, true, &tmp_err);
-  crun_error_release (&tmp_err);
 
   if (init_pid)
     kill (init_pid, SIGKILL);
@@ -1631,7 +1629,7 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
 
     ret = libcrun_cgroup_enter (&cg, err);
     if (UNLIKELY (ret < 0))
-      return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+      return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
     if (def->linux && def->linux->resources)
       {
@@ -1639,19 +1637,19 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
                                                def->linux->resources,
                                                cgroup_path, err);
         if (UNLIKELY (ret < 0))
-          return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+          return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
       }
   }
 
   /* sync 1.  */
   ret = sync_socket_send_sync (sync_socket, true, err);
   if (UNLIKELY (ret < 0))
-    return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+    return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
   /* sync 2.  */
   ret = sync_socket_wait_sync (context, sync_socket, false, err);
   if (UNLIKELY (ret < 0))
-    return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+    return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
   /* The container is waiting that we write back.  In this phase we can launch the
      prestart hooks.  */
@@ -1661,7 +1659,7 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
                       (hook **) def->hooks->prestart,
                       def->hooks->prestart_len, hooks_out_fd, hooks_err_fd, err);
       if (UNLIKELY (ret != 0))
-        return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+        return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
     }
   if (def->hooks && def->hooks->create_runtime_len)
     {
@@ -1669,7 +1667,7 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
                       (hook **) def->hooks->create_runtime,
                       def->hooks->create_runtime_len, hooks_out_fd, hooks_err_fd, err);
       if (UNLIKELY (ret != 0))
-        return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+        return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
     }
 
   if (seccomp_fd >= 0)
@@ -1683,41 +1681,41 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
 
       ret = libcrun_generate_seccomp (container, seccomp_fd, seccomp_gen_options, err);
       if (UNLIKELY (ret < 0))
-        return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+        return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
       close_and_reset (&seccomp_fd);
     }
 
   /* sync 3.  */
   ret = sync_socket_send_sync (sync_socket, true, err);
   if (UNLIKELY (ret < 0))
-    return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+    return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
   if (def->process && def->process->terminal && !detach && context->console_socket == NULL)
     {
       terminal_fd = receive_fd_from_socket (socket_pair_0, err);
       if (UNLIKELY (terminal_fd < 0))
-        return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+        return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
       close_and_reset (&socket_pair_0);
 
       ret = libcrun_setup_terminal_master (terminal_fd, &orig_terminal, err);
       if (UNLIKELY (ret < 0))
-        return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+        return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
     }
 
   /* sync 4.  */
   ret = sync_socket_wait_sync (context, sync_socket, false, err);
   if (UNLIKELY (ret < 0))
-    return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+    return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
   ret = close_and_reset (&sync_socket);
   if (UNLIKELY (ret < 0))
-    return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+    return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
   get_current_timestamp (created);
   ret = write_container_status (container, context, pid, cgroup_path, scope, created, err);
   if (UNLIKELY (ret < 0))
-    return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+    return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
 
   if (def->hooks && def->hooks->poststart_len)
     {
@@ -1725,13 +1723,13 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
                       (hook **) def->hooks->poststart,
                       def->hooks->poststart_len, hooks_out_fd, hooks_err_fd, err);
       if (UNLIKELY (ret < 0))
-        return cleanup_watch (context, pid, def, context->id, sync_socket, terminal_fd, err);
+        return cleanup_watch (context, pid, sync_socket, terminal_fd, err);
     }
 
   ret = wait_for_process (pid, context, terminal_fd, notify_socket, container_ready_fd, err);
   if (!context->detach)
     {
-      cleanup_watch (context, 0, def, context->id, sync_socket, terminal_fd, err);
+      cleanup_watch (context, 0, sync_socket, terminal_fd, err);
       crun_error_release (err);
     }
 
@@ -1778,6 +1776,14 @@ int libcrun_copy_config_file (const char *id, const char *state_root, const char
   return 0;
 }
 
+static void
+force_delete_container_status (libcrun_context_t *context, runtime_spec_schema_config_schema *def)
+{
+  libcrun_error_t tmp_err = NULL;
+  container_delete_internal (context, def, context->id, 1, true, &tmp_err);
+  crun_error_release (&tmp_err);
+}
+
 int
 libcrun_container_run (libcrun_context_t *context, libcrun_container_t *container, unsigned int options, libcrun_error_t *err)
 {
@@ -1809,7 +1815,11 @@ libcrun_container_run (libcrun_context_t *context, libcrun_container_t *containe
       ret = libcrun_copy_config_file (context->id, context->state_root, context->bundle, err);
       if (UNLIKELY (ret < 0))
         return ret;
-      return libcrun_container_run_internal (container, context, -1, err);
+
+      ret = libcrun_container_run_internal (container, context, -1, err);
+      if (UNLIKELY (ret < 0))
+        force_delete_container_status (context, def);
+      return ret;
     }
 
   ret = pipe (container_ret_status);
@@ -1865,6 +1875,7 @@ libcrun_container_run (libcrun_context_t *context, libcrun_container_t *containe
   TEMP_FAILURE_RETRY (write (pipefd1, &ret, sizeof (ret)));
   if (UNLIKELY (ret < 0))
     {
+      force_delete_container_status (context, def);
       TEMP_FAILURE_RETRY (write (pipefd1, &((*err)->status), sizeof ((*err)->status)));
       TEMP_FAILURE_RETRY (write (pipefd1, (*err)->msg, strlen ((*err)->msg) + 1));
     }
@@ -1910,7 +1921,10 @@ libcrun_container_create (libcrun_context_t *context, libcrun_container_t *conta
       ret = libcrun_copy_config_file (context->id, context->state_root, context->bundle, err);
       if (UNLIKELY (ret < 0))
         return ret;
-      return libcrun_container_run_internal (container, context, -1, err);
+      ret = libcrun_container_run_internal (container, context, -1, err);
+      if (UNLIKELY (ret < 0))
+        force_delete_container_status (context, def);
+      return ret;
     }
 
   ret = pipe (container_ready_pipe);
@@ -1957,6 +1971,7 @@ libcrun_container_create (libcrun_context_t *context, libcrun_container_t *conta
   ret = libcrun_container_run_internal (container, context, pipefd1, err);
   if (UNLIKELY (ret < 0))
     {
+      force_delete_container_status (context, def);
       libcrun_error ((*err)->status, "%s", (*err)->msg);
       crun_set_output_handler (log_write_to_stderr, NULL, false);
     }

commit 9085e62fa2d540f1e5b2b08f8b08143a3c5df064

container, run: always delete container without detach
regression introduced by 7179d71afd608f5e85740b2b4b83f9b41fbe1b15

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/container.c
@@ -1819,8 +1819,7 @@ libcrun_container_run (libcrun_context_t *context, libcrun_container_t *containe
         return ret;
 
       ret = libcrun_container_run_internal (container, context, -1, err);
-      if (UNLIKELY (ret < 0))
-        force_delete_container_status (context, def);
+      force_delete_container_status (context, def);
       return ret;
     }
 

tests/test_start.py
@@ -46,8 +46,23 @@ def test_start():
             run_crun_command(["delete", "-f", cid])
     return 0
 
+def test_run_twice():
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'echo', 'hi']
+    add_all_namespaces(conf)
+    try:
+        id_container = "container-%s" % os.getpid()
+        for i in range(2):
+            out, cid = run_and_get_output(conf, command='run', id_container=id_container)
+            if "hi" not in str(out):
+                return -1
+    except:
+        return -1
+    return 0
+
 all_tests = {
     "start" : test_start,
+    "run-twice" : test_run_twice,
 }
 
 if __name__ == "__main__":

tests/tests_utils.py
@@ -187,7 +187,7 @@ def get_crun_path():
 
 def run_and_get_output(config, detach=False, preserve_fds=None, pid_file=None,
                        command='run', use_popen=False, hide_stderr=False,
-                       all_dev_null=False):
+                       all_dev_null=False, id_container=None):
     temp_dir = tempfile.mkdtemp(dir=get_tests_root())
     rootfs = os.path.join(temp_dir, "rootfs")
     os.makedirs(rootfs)
@@ -195,7 +195,9 @@ def run_and_get_output(config, detach=False, preserve_fds=None, pid_file=None,
         os.makedirs(os.path.join(rootfs, i))
     with open(os.path.join(rootfs, "var", "file"), "w+") as f:
         f.write("file")
-    id_container = 'test-%s' % os.path.basename(temp_dir)
+
+    if id_container is None:
+        id_container = 'test-%s' % os.path.basename(temp_dir)
 
     with open(os.path.join(temp_dir, "config.json"), "w") as config_file:
         conf = json.dumps(config)