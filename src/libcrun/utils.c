containers/crun

commit 99538d805bb62fbda6e82627d8daa0b90c97a8d0

utils: resolve last component if symlink
Closes: https://github.com/containers/crun/issues/497

Signed-off-by: Giuseppe Scrivano <giuseppe@scrivano.org>

src/libcrun/utils.c
@@ -48,6 +48,8 @@
 #  define __NR_openat2 437
 #endif
 
+#define MAX_READLINKS 32
+
 static int
 syscall_openat2 (int dirfd, const char *path, uint64_t flags, uint64_t mode, uint64_t resolve)
 {
@@ -367,15 +369,19 @@ fallback:
 
 static int
 crun_safe_ensure_at (bool dir, int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode,
-                     libcrun_error_t *err)
+                     int max_readlinks, libcrun_error_t *err)
 {
   cleanup_close int wd_cleanup = -1;
   cleanup_free char *npath = NULL;
   bool last_component = false;
+  size_t depth = 0;
   const char *cur;
   char *it;
   int cwd;
   int ret;
+
+  if (max_readlinks <= 0)
+    return crun_make_error (err, ELOOP, "resolve path `%s`", path);
 
   while (*path == '/')
     path++;
@@ -401,8 +407,20 @@ crun_safe_ensure_at (bool dir, int dirfd, const char *dirpath, size_t dirpath_le
       if (cur[0] == '\0')
         break;
 
-      if (strcmp (cur, "..") == 0)
-        return crun_make_error (err, 0, "invalid path `%s`", path);
+      if (strcmp (cur, ".") == 0)
+        goto next;
+      else if (strcmp (cur, ".."))
+        depth++;
+      else
+        {
+          if (depth)
+            depth--;
+          else
+            {
+              close_and_reset (&wd_cleanup);
+              cwd = dirfd;
+            }
+        }
 
       if (! last_component || dir)
         ret = mkdirat (cwd, cur, mode);
@@ -411,6 +429,29 @@ crun_safe_ensure_at (bool dir, int dirfd, const char *dirpath, size_t dirpath_le
           ret = openat (cwd, cur, O_CLOEXEC | O_CREAT | O_WRONLY | O_NOFOLLOW, 0700);
           if (UNLIKELY (ret < 0))
             {
+              /* If the last component is a symlink, repeat the lookup with the resolved path.  */
+              if (errno == ELOOP)
+                {
+                  size_t s, resolved_size = 0;
+                  cleanup_free char *resolved_path = NULL;
+
+                  do
+                    {
+                      resolved_size += 512;
+                      resolved_path = xrealloc (resolved_path, resolved_size);
+
+                      s = readlinkat (dirfd, npath, resolved_path, resolved_size);
+                    }
+                  while (s == resolved_size);
+
+                  if (s > 0)
+                    {
+                      resolved_path[s] = '\0';
+                      crun_error_release (err);
+                      return crun_safe_ensure_at (dir, dirfd, dirpath, dirpath_len, resolved_path, mode,
+                                                  max_readlinks - 1, err);
+                    }
+                }
               /* If the previous openat fails, attempt to open the file in O_PATH mode.  */
               ret = openat (cwd, cur, O_CLOEXEC | O_PATH, 0);
               if (ret < 0)
@@ -434,6 +475,7 @@ crun_safe_ensure_at (bool dir, int dirfd, const char *dirpath, size_t dirpath_le
 
       close_and_replace (&wd_cleanup, cwd);
 
+    next:
       if (it == NULL)
         break;
 
@@ -449,14 +491,14 @@ int
 crun_safe_ensure_directory_at (int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode,
                                libcrun_error_t *err)
 {
-  return crun_safe_ensure_at (true, dirfd, dirpath, dirpath_len, path, mode, err);
+  return crun_safe_ensure_at (true, dirfd, dirpath, dirpath_len, path, mode, MAX_READLINKS, err);
 }
 
 int
 crun_safe_ensure_file_at (int dirfd, const char *dirpath, size_t dirpath_len, const char *path, int mode,
                           libcrun_error_t *err)
 {
-  return crun_safe_ensure_at (false, dirfd, dirpath, dirpath_len, path, mode, err);
+  return crun_safe_ensure_at (false, dirfd, dirpath, dirpath_len, path, mode, MAX_READLINKS, err);
 }
 
 int

tests/test_mounts.py
@@ -56,6 +56,17 @@ def test_mount_symlink():
         return 0
     return -1
 
+def test_mount_symlink_not_existing():
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'cat', '/proc/self/mountinfo']
+    add_all_namespaces(conf)
+    mount_opt = {"destination": "/etc/not-existing", "type": "bind", "source": "/etc/localtime", "options": ["bind", "ro"]}
+    conf['mounts'].append(mount_opt)
+    out, _ = run_and_get_output(conf, hide_stderr=True)
+    if "foo/bar" in out:
+        return 0
+    return -1
+
 def test_mount_ro():
     a = helper_mount("ro")[0]
     if "ro" in a:
@@ -128,6 +139,7 @@ all_tests = {
     "test-mount-sync" : test_mount_sync,
     "test-mount-dirsync" : test_mount_dirsync,
     "test-mount-symlink" : test_mount_symlink,
+    "test-mount-symlink-not-existing" : test_mount_symlink_not_existing,
 }
 
 if __name__ == "__main__":

tests/tests_utils.py
@@ -220,6 +220,7 @@ def run_and_get_output(config, detach=False, preserve_fds=None, pid_file=None,
 
     open(os.path.join(rootfs, "usr/share/zoneinfo/Europe/Rome"), "w").close()
     os.symlink("../usr/share/zoneinfo/Europe/Rome", os.path.join(rootfs, "etc/localtime"))
+    os.symlink("../foo/bar/not/here", os.path.join(rootfs, "etc/not-existing"))
 
     detach_arg = ['--detach'] if detach else []
     preserve_fds_arg = ['--preserve-fds', str(preserve_fds)] if preserve_fds else []

commit 3ce74e87c12be156537f66efe8028c0ad2e6fec2

utils: fix symlink lookup
commit 99538d805bb62fbda6e82627d8daa0b90c97a8d0 introduced this
regression.

This issue happens only on old kernel that lack openat2.

The error happens with a command like:

$ podman run --rm -v /etc/localtime:/etc/localtime ubi8 echo "This works"
Error: open `..`: No such file or directory: OCI not found

We already have tests in place to detect this case, but they were
running on newer kernels so the code path was completely skipped.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/utils.c
@@ -362,6 +362,15 @@ fallback:
   while (*path_in_chroot == '/')
     path_in_chroot++;
 
+  /* If the path is empty we are at the root, dup the dirfd itself.  */
+  if (path_in_chroot[0] == '\0')
+    {
+      ret = dup (dirfd);
+      if (UNLIKELY (ret < 0))
+        return crun_make_error (err, errno, "dup `%s`", rootfs);
+      return ret;
+    }
+
   ret = openat (dirfd, path_in_chroot, flags, mode);
   if (UNLIKELY (ret < 0))
     return crun_make_error (err, errno, "open `%s`", path);