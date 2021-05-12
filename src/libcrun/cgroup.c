2de8b43435ad406da8061b3b44945ca79f37bfba
flouthoc <flouthoc.git@gmail.com>
2021-05-12 11:21:34 +0530

    Fix SIGSEGV for rootless container caused by case when def->linux is defined but def->linux->cgroups_path is NULL
    Signed-off-by: flouthoc <flouthoc.git@gmail.com>

--- src/libcrun/cgroup.c
+++ src/libcrun/cgroup.c
@@ -1856,7 +1856,7 @@ libcrun_cgroup_enter (struct libcrun_cgroup_args *args, libcrun_error_t *err)
     }
 
   /* Ignore errors only if there is no explicit path set in the configuration.  */
-  cgroup_path_empty = args->cgroup_path[0] == '\0';
+  cgroup_path_empty = (args->cgroup_path == NULL || args->cgroup_path[0] == '\0');
   if (rootless > 0 && cgroup_path_empty && (cgroup_mode != CGROUP_MODE_UNIFIED || manager != CGROUP_MANAGER_SYSTEMD))
     {
       /* Ignore cgroups errors and set there is no cgroup path to use.  */
