containers/crun

commit 52019563aa5732414c00a91b61add7f3d75464ea

container: apply SELinux label to keyring
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -298,19 +298,55 @@ syscall_pidfd_send_signal (int pidfd, int sig, siginfo_t *info, unsigned int fla
 }
 
 int
-libcrun_create_keyring (const char *name, libcrun_error_t *err)
+libcrun_create_keyring (const char *name, const char *label, libcrun_error_t *err)
 {
-  int ret = syscall_keyctl_join (name);
+  const char *const keycreate = "/proc/self/attr/keycreate";
+  cleanup_close int labelfd = -1;
+  bool label_set = false;
+  int ret;
+
+  if (label)
+    {
+      labelfd = open (keycreate, O_WRONLY | O_CLOEXEC);
+      if (UNLIKELY (labelfd < 0))
+        {
+          if (errno != ENOENT)
+            return crun_make_error (err, errno, "open `%s`", keycreate);
+        }
+      else
+        {
+          ret = write (labelfd, label, strlen (label));
+          if (UNLIKELY (ret < 0))
+            return crun_make_error (err, errno, "write to `%s`", keycreate);
+
+          label_set = true;
+        }
+    }
+
+  ret = syscall_keyctl_join (name);
   if (UNLIKELY (ret < 0))
     {
       if (errno == ENOSYS)
         {
           libcrun_warning ("could not create a new keyring: keyctl_join is not supported");
-          return 0;
+          ret = 0;
+          goto out;
         }
-      return crun_make_error (err, errno, "create keyring `%s`", name);
+      ret = crun_make_error (err, errno, "create keyring `%s`", name);
+      goto out;
     }
-  return 0;
+
+out:
+  if (label_set)
+    {
+      int tmp_ret;
+
+      tmp_ret = write (labelfd, "", 0);
+      /* do not override a previous error.  */
+      if (UNLIKELY (tmp_ret < 0 && ret >= 0))
+        return crun_make_error (err, errno, "reset `%s`", keycreate);
+    }
+  return ret;
 }
 
 static void

src/libcrun/container.c
@@ -2218,7 +2218,12 @@ libcrun_container_run_internal (libcrun_container_t *container, libcrun_context_
 
   if (! context->no_new_keyring)
     {
-      ret = libcrun_create_keyring (container->context->id, err);
+      const char *label = NULL;
+
+      if (def->process)
+        label = def->process->selinux_label;
+
+      ret = libcrun_create_keyring (container->context->id, label, err);
       if (UNLIKELY (ret < 0))
         return ret;
     }

src/libcrun/linux.h
@@ -55,7 +55,7 @@ int libcrun_join_process (libcrun_container_t *container, pid_t pid_to_join, lib
                           int detach, int *terminal_fd, libcrun_error_t *err);
 int libcrun_linux_container_update (libcrun_container_status_t *status, const char *content, size_t len,
                                     libcrun_error_t *err);
-int libcrun_create_keyring (const char *name, libcrun_error_t *err);
+int libcrun_create_keyring (const char *name, const char *label, libcrun_error_t *err);
 int libcrun_container_pause_linux (libcrun_container_status_t *status, libcrun_error_t *err);
 int libcrun_container_unpause_linux (libcrun_container_status_t *status, libcrun_error_t *err);
 int libcrun_container_enter_cgroup_ns (libcrun_container_t *container, libcrun_error_t *err);

commit 923447b691dbd7c5bffbaee1427460d62d848047

container: ignore resetting keyring SELinux label
ignore the error when resetting the SELinux label for newly created
keyrings.

If an error happen the label is left configured to the container
process SELinux label.

commit 52019563aa5732414c00a91b61add7f3d75464ea introduced the issue.

Closes: https://github.com/containers/crun/issues/687

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -337,15 +337,9 @@ libcrun_create_keyring (const char *name, const char *label, libcrun_error_t *er
     }
 
 out:
+  /* Best effort attempt to reset the SELinux label used for new keyrings.  */
   if (label_set)
-    {
-      int tmp_ret;
-
-      tmp_ret = write (labelfd, "", 0);
-      /* do not override a previous error.  */
-      if (UNLIKELY (tmp_ret < 0 && ret >= 0))
-        return crun_make_error (err, errno, "reset `%s`", keycreate);
-    }
+    write (labelfd, "", 0);
   return ret;
 }
 