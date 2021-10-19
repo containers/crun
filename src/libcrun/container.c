containers/crun

commit 979f6f02f673bc6f7b70cdfb03669eb9682d7296

criu: save the new descriptors after restore
after a restore, save the new descriptors instead of re-using the old
ones.

Closes: https://github.com/containers/crun/issues/756

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -2700,8 +2700,8 @@ libcrun_get_external_descriptors (libcrun_container_t *container)
   return get_private_data (container)->external_descriptors;
 }
 
-static int
-save_external_descriptors (libcrun_container_t *container, pid_t pid, libcrun_error_t *err)
+int
+libcrun_save_external_descriptors (libcrun_container_t *container, pid_t pid, libcrun_error_t *err)
 {
   const unsigned char *buf = NULL;
   yajl_gen gen = NULL;
@@ -3458,7 +3458,7 @@ libcrun_run_linux_container (libcrun_container_t *container, container_entrypoin
     {
       cleanup_pid pid_t pid_to_clean = pid;
 
-      ret = save_external_descriptors (container, pid, err);
+      ret = libcrun_save_external_descriptors (container, pid, err);
       if (UNLIKELY (ret < 0))
         return ret;
 

src/libcrun/criu.c
@@ -543,8 +543,6 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
     if (UNLIKELY (ret < 0))
       return ret;
 
-    status->external_descriptors = xstrdup (buffer);
-
     /* descriptors.json contains a JSON array with strings
      * telling where 0, 1 and 2 have been initially been
      * pointing to. For each descriptor which points to
@@ -750,10 +748,16 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
    * be necessary later when moving the process into its cgroup. */
   status->pid = ret;
 
+  if (LIKELY (ret > 0))
+    ret = libcrun_save_external_descriptors (container, ret, err);
+
 out_umount:
   ret_out = umount (root);
   if (UNLIKELY (ret_out == -1))
-    return crun_make_error (err, errno, "error unmounting restore directory %s", root);
+    {
+      rmdir (root);
+      return crun_make_error (err, errno, "error unmounting restore directory %s", root);
+    }
 out:
   ret_out = rmdir (root);
   if (UNLIKELY (ret == -1))

src/libcrun/linux.h
@@ -76,4 +76,5 @@ int libcrun_container_setgroups (libcrun_container_t *container,
 int libcrun_kill_linux (libcrun_container_status_t *status, int signal, libcrun_error_t *err);
 int libcrun_create_final_userns (libcrun_container_t *container, libcrun_error_t *err);
 int libcrun_create_kvm_device (libcrun_container_t *container, libcrun_error_t *err);
+int libcrun_save_external_descriptors (libcrun_container_t *container, pid_t pid, libcrun_error_t *err);
 #endif

commit fd7b3cb07c37d8c73837c549f51ad145af91565d

criu: do not override external_descriptors
libcrun_container_restore_linux_criu already sets the external
descriptors.

commit 979f6f02f673bc6f7b70cdfb03669eb9682d7296 introduced the
regression.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/container.c
@@ -1756,6 +1756,8 @@ write_container_status (libcrun_container_t *container, libcrun_context_t *conte
                                         .external_descriptors = external_descriptors };
   if (cwd == NULL)
     OOM ();
+  if (external_descriptors == NULL)
+    return crun_make_error (err, 0, "invalid internal state.  No external descriptors found");
   return libcrun_write_container_status (context->state_root, context->id, &status, err);
 }
 

src/libcrun/linux.c
@@ -3987,8 +3987,6 @@ libcrun_container_restore_linux (libcrun_container_status_t *status, libcrun_con
   if (UNLIKELY (ret < 0))
     return ret;
 
-  get_private_data (container)->external_descriptors = status->external_descriptors;
-
   return 0;
 }
 