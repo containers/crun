6457228867831546fff0572952765401dae769bd
Giuseppe Scrivano <gscrivan@redhat.com>
2021-12-15 10:00:12 +0100

    tests: add CRI-O integration tests to the CI
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

---
+++ tests/cri-o/run-tests.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cd $1
+
+if test "$(id -u)" != 0; then
+	echo "run as root"
+	exit 1
+fi
+
+(cd /crun; git clean -fdx; ./autogen.sh && ./configure CFLAGS='-Wall -Wextra -Werror' --prefix=/usr && make -j $(nproc) && make install)
+
+# make sure runc is not used
+rm -f /usr/bin/runc /usr/local/bin/runc
+ln /usr/bin/crun /usr/bin/runc
+
+rm -f /usr/bin/journalctl
+
+export CONTAINER_CGROUP_MANAGER=cgroupfs
+export RUNTIME_BINARY_PATH=/usr/bin/crun
+export CONTAINER_CONMON_CGROUP=pod
+export JOBS=2
+
+# adapt tests to cgroupfs
+sed -i 's/\.slice//g' test/testdata/*.json
+sed -i -e 's|@test "conmon custom cgroup" {|@test "conmon custom cgroup" {\nskip\n|g' test/*.bats
+sed -i -e 's|@test "privileged ctr device add" {|@test "privileged ctr device add" {\nskip\n|g' test/*.bats
+sed -i -e 's|@test "ctr device add" {|@test "ctr device add" {\nskip\n|g' test/*.bats
+sed -i -e 's|@test "privileged ctr -- check for rw mounts" {|@test "privileged ctr -- check for rw mounts" {\nskip\n|g' test/*.bats
+sed -i -e 's|@test "kubernetes pod terminationGracePeriod passthru" {|@test "kubernetes pod terminationGracePeriod passthru" {\nskip\n|g' test/*.bats
+
+# remove unuseful tests
+rm test/image.* test/config* test/reload_config.bats test/registries.bats test/crio-wipe.bats test/network_ping.bats
+
+test/test_runner.sh
