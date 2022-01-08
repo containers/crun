ff3e33b95eba65f5cea89e9070ca1677a0a953d7
Kir Kolyshkin <kolyshkin@gmail.com>
2022-01-07 18:31:47 -0800

    tests/fuzzing: nits
    This fixes a few nits:
    
     - shell code "(grep -v xxx || exit 1)" is equivalent to "grep -v xxx",
       except no subshell is used;
    
     - TEST_CASES renamed to TEST_CASE.
    
    This also fixes a few shellcheck warnings:
    
     - use bash arithmetics instead of expr;
    
     - quote some arguments shellcheck comp.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- tests/fuzzing/run-tests.sh
+++ tests/fuzzing/run-tests.sh
@@ -8,27 +8,26 @@ VERBOSITY=${VERBOSITY:=}
 
 N_TESTS=7
 
-SINGLE_RUN_TIME=$(expr $RUN_TIME / $N_TESTS)
+SINGLE_RUN_TIME=$(( RUN_TIME / N_TESTS ))
 
 CORPUS=${CORPUS:=/testcases}
 
 cd /crun
-
 git clean -fdx
 ./autogen.sh
 ./configure --enable-embedded-yajl HFUZZ_CC_UBSAN=1 HFUZZ_CC_ASAN=1 CC=hfuzz-clang CPPFLAGS="-D FUZZER" CFLAGS="-ggdb3 -fsanitize-coverage=trace-pc-guard,trace-cmp,trace-div,indirect-calls"
-make -j $(nproc)
+make -j "$(nproc)"
 
 mkdir rootfs
 mkdir random-data
 
 function run_test {
     export FUZZING_MODE=$1
-    TEST_CASES=$2
+    TEST_CASE=$2
 
-    result=$(honggfuzz --exit_upon_crash $VERBOSITY --run_time $SINGLE_RUN_TIME --timeout $TIMEOUT -T -i $TEST_CASES -- tests/tests_libcrun_fuzzer 2>&1 | tail -n 2)
-    echo $result
-    echo $result | (grep -q crashes_count:0 || exit 1)
+    result=$(honggfuzz --exit_upon_crash $VERBOSITY --run_time "$SINGLE_RUN_TIME" --timeout $TIMEOUT -T -i "$TEST_CASE" -- tests/tests_libcrun_fuzzer 2>&1 | tail -n 2)
+    echo "$result"
+    echo "$result" | grep -q crashes_count:0
 }
 
 run_test 0 $CORPUS/config-json
