containers/crun

commit 21a8dafae7f26cba1250519aaeb3e55ea445cf93

libcrun: let libcrun_container_kill* accept a string
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

python/crun_python.c
@@ -233,11 +233,11 @@ container_kill (PyObject *self, PyObject *args)
   libcrun_error_t err;
   PyObject *ctx_obj = NULL;
   char *id = NULL;
-  int signal;
+  const char signal*;
   libcrun_context_t *ctx;
   int ret;
 
-  if (!PyArg_ParseTuple (args, "Osi", &ctx_obj, &id, &signal))
+  if (!PyArg_ParseTuple (args, "Oss", &ctx_obj, &id, &signal))
     return NULL;
 
   ctx = PyCapsule_GetPointer (ctx_obj, CONTEXT_OBJ_TAG);

src/kill.c
@@ -87,7 +87,8 @@ static struct argp run_argp = { options, parse_opt, args_doc, doc, NULL, NULL, N
 int
 crun_command_kill (struct crun_global_arguments *global_args, int argc, char **argv, libcrun_error_t *err)
 {
-  int first_arg = 0, signal, ret;
+  int first_arg = 0, ret;
+  const char *signal;
 
   libcrun_context_t crun_context = {
     0,
@@ -100,13 +101,9 @@ crun_command_kill (struct crun_global_arguments *global_args, int argc, char **a
   if (UNLIKELY (ret < 0))
     return ret;
 
-  signal = SIGTERM;
+  signal = "SIGTERM";
   if (argc - first_arg > 1)
-    {
-      signal = libcrun_str2sig (argv[first_arg + 1]);
-      if (UNLIKELY (signal < 0))
-        libcrun_fail_with_error (0, "unknown signal %s", argv[first_arg + 1]);
-    }
+    signal = argv[first_arg + 1];
 
   if (kill_options.regex)
     {

src/libcrun/container.c
@@ -1613,26 +1613,34 @@ libcrun_container_delete (libcrun_context_t *context, runtime_spec_schema_config
 }
 
 int
-libcrun_container_kill (libcrun_context_t *context, const char *id, int signal, libcrun_error_t *err)
+libcrun_container_kill (libcrun_context_t *context, const char *id, const char *signal, libcrun_error_t *err)
 {
-  int ret;
+  int sig, ret;
   const char *state_root = context->state_root;
   cleanup_container_status libcrun_container_status_t status = {};
+
+  sig = libcrun_str2sig (signal);
+  if (UNLIKELY (sig < 0))
+    return crun_make_error (err, 0, "unknown signal %s", signal);
 
   ret = libcrun_read_container_status (&status, state_root, id, err);
   if (UNLIKELY (ret < 0))
     return ret;
 
-  return libcrun_kill_linux (&status, signal, err);
+  return libcrun_kill_linux (&status, sig, err);
 }
 
 int
-libcrun_container_kill_all (libcrun_context_t *context, const char *id, int signal, libcrun_error_t *err)
+libcrun_container_kill_all (libcrun_context_t *context, const char *id, const char *signal, libcrun_error_t *err)
 {
-  int ret;
+  int sig, ret;
   const char *state_root = context->state_root;
   cleanup_container_status libcrun_container_status_t status = {};
   cleanup_cgroup_status struct libcrun_cgroup_status *cgroup_status = NULL;
+
+  sig = libcrun_str2sig (signal);
+  if (UNLIKELY (sig < 0))
+    return crun_make_error (err, 0, "unknown signal %s", signal);
 
   ret = libcrun_read_container_status (&status, state_root, id, err);
   if (UNLIKELY (ret < 0))
@@ -1640,7 +1648,7 @@ libcrun_container_kill_all (libcrun_context_t *context, const char *id, int sign
 
   cgroup_status = libcrun_cgroup_make_status (&status);
 
-  ret = libcrun_cgroup_killall (cgroup_status, signal, err);
+  ret = libcrun_cgroup_killall (cgroup_status, sig, err);
   if (UNLIKELY (ret < 0))
     return ret;
   return 0;

src/libcrun/container.h
@@ -111,10 +111,10 @@ LIBCRUN_PUBLIC int libcrun_container_run (libcrun_context_t *context, libcrun_co
 LIBCRUN_PUBLIC int libcrun_container_delete (libcrun_context_t *context, runtime_spec_schema_config_schema *def,
                                              const char *id, bool force, libcrun_error_t *err);
 
-LIBCRUN_PUBLIC int libcrun_container_kill (libcrun_context_t *context, const char *id, int signal,
+LIBCRUN_PUBLIC int libcrun_container_kill (libcrun_context_t *context, const char *id, const char *signal,
                                            libcrun_error_t *err);
 
-LIBCRUN_PUBLIC int libcrun_container_kill_all (libcrun_context_t *context, const char *id, int signal,
+LIBCRUN_PUBLIC int libcrun_container_kill_all (libcrun_context_t *context, const char *id, const char *signal,
                                                libcrun_error_t *err);
 
 LIBCRUN_PUBLIC int libcrun_container_create (libcrun_context_t *context, libcrun_container_t *container,

commit 2f5be742a283b890925ae7070fe8bfa0e4d00f5d

python: fix build
commit 21a8dafae7f26cba1250519aaeb3e55ea445cf93 introduced the
regression.

Closes: https://github.com/containers/crun/issues/854

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

python/crun_python.c
@@ -233,7 +233,7 @@ container_kill (PyObject *self, PyObject *args)
   libcrun_error_t err;
   PyObject *ctx_obj = NULL;
   char *id = NULL;
-  const char signal*;
+  char *signal;
   libcrun_context_t *ctx;
   int ret;
 