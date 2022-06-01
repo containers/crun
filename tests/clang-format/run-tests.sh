38f60b15c93e20dacf0fb3708653503c3a4fae50
Kir Kolyshkin <kolyshkin@gmail.com>
2022-06-01 16:51:36 -0700

    ci: re-enable and fix clang-format
    1. The check was a no-op since commit a51137c91ad which neglected to add
       the ENTRYPOINT in Dockerfile.
    
    2. The above commit also forgot to install git. While at it, s/yum/dnf/.
    
    3. Since a recent git release, any git operation working on files not
       belonging to a current use complains like this:
    
    	fatal: unsafe repository ('/crun' is owned by someone else)
    
       To workaround that, add git config --add safe.directory.
    
    4. Differences in tests subdirectory were not caught because commit
       4b184254 neglected to add tests to git diff --exit-code arguments.
       Replace the list of directories to check with --ignore-submodules,
       so we don't have to keep the list of directories in Makefile's
       clang-format git ls-files with the one in run-tests.sh here.
    
    Fixes: a51137c91ad3e8ce9f7e2a19334b96a05bba5d2b
    Fixes: 4b184254ff82449adffea79b2da03432488a17dd
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- tests/clang-format/run-tests.sh
+++ tests/clang-format/run-tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-set -e
+set -e -x
+
+cd /crun
+
+# Recent git complains that the directory is owned by someone else,
+# which happens if we run it via a Dockefile with a volume mounted.
+git config --global --add safe.directory "$(pwd)"
 
 ./configure
 make clang-format
-git diff --exit-code src
+git diff --ignore-submodules --exit-code
