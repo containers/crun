8ff3eba180736b7be61b09b2ad42d2fd08180af7
Lokesh Mandvekar <lsm5@fedoraproject.org>
2022-07-29 15:22:21 -0400

    rpm/Makefile: Fix copr build (follow-up on #979)
    - `make -f .copr/Makefile srpm outdir=$(OUTDIR)` works.
    See: https://docs.pagure.org/copr.copr/user_documentation.html#make-srpm
    
    - `./autogen.sh && ./configure && make [srpm,rpm]` works locally.
    
    - added Fedora version conditionals for libkrun support as
    libkrun exists only for Fedora 37/rawhide on aarch64 and x86_64.
    
    - `./autogen.sh` and `./configure` need to be run in .copr/Makefile for
    `srpm`, `rpm` and `libocispec` targets as well as in `.copr/crun.spec` after
    exploding the tarball for generating the actual rpms.
    
    - rpm build release tag is of the form:
    `$TIMESTAMP.$GIT_SHORTCOMMIT.$DIST`.
    
    - `.copr/crun.spec.in` temporarily uses hardcoded `Version: 0.0` as copr seems to
    use a strange and unexpected string for `$VERSION` and thus `@RPM_VERSION@`.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- tests/fedora-rawhide-mockbuild/run-mockbuild.sh
+++ tests/fedora-rawhide-mockbuild/run-mockbuild.sh
@@ -2,11 +2,9 @@
 
 set -e
 cd /crun
-
 # needed to silence ambiguous directory ownership error
 git config --global --add safe.directory /crun
 git clean -fdx
-git submodule foreach --recursive git clean -fdx
 
-cd .copr
-make srpm
+make -f .copr/Makefile srpm
+make rpm
