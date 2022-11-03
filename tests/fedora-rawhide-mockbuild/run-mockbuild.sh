5308c49121b85905f36a1e736adc09ded38a0753
Lokesh Mandvekar <lsm5@fedoraproject.org>
2022-11-03 18:12:10 +0530

    copr: crun-wasm subpackage
    crun will now be built with both wasmedge and wasmtime support.
    
    wasmtime on podman-next copr now provides wasm-library and a PR has been
    opened on the wasmedge Fedora package to do the same.
    
    wasmedge will be the default wasm provider installed.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- tests/fedora-rawhide-mockbuild/run-mockbuild.sh
+++ tests/fedora-rawhide-mockbuild/run-mockbuild.sh
@@ -11,5 +11,5 @@ make -f .copr/Makefile srpm
 dnf -y install 'dnf-command(copr)'
 # Copr repo subject to change after discussions with wasmtime upstream
 dnf -y copr enable lsm5/wasmtime
-dnf -y install libkrun-devel wasmtime-c-api-devel
+dnf -y install libkrun-devel wasmedge-devel wasmtime-c-api-devel
 make rpm
