containers/crun

commit 0efbe561afb40023872bcc2db84c701a73dc4dc9

utils: change AppArmor profile for the current proc
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/utils.c
@@ -838,7 +838,7 @@ libcrun_is_apparmor_enabled (libcrun_error_t *err)
 }
 
 int
-set_apparmor_profile (const char *profile, libcrun_error_t *err)
+set_apparmor_profile (const char *profile, bool now, libcrun_error_t *err)
 {
   int ret;
 
@@ -847,11 +847,12 @@ set_apparmor_profile (const char *profile, libcrun_error_t *err)
     return ret;
   if (ret)
     {
+      const char *fname = now ? "/proc/thread-self/attr/current" : "/proc/thread-self/attr/exec";
       cleanup_free char *buf = NULL;
 
       xasprintf (&buf, "exec %s", profile);
 
-      ret = write_file_and_check_fs_type ("/proc/thread-self/attr/exec", buf, strlen (buf), PROC_SUPER_MAGIC, "procfs",
+      ret = write_file_and_check_fs_type (fname, buf, strlen (buf), PROC_SUPER_MAGIC, "procfs",
                                           err);
       if (UNLIKELY (ret < 0))
         return ret;

src/libcrun/container.c
@@ -1047,7 +1047,7 @@ container_init_setup (void *args, pid_t own_pid, char *notify_socket,
       if (UNLIKELY (ret < 0))
         return ret;
 
-      ret = libcrun_set_apparmor_profile (def->process, err);
+      ret = libcrun_set_apparmor_profile (def->process, false, err);
       if (UNLIKELY (ret < 0))
         return ret;
     }
@@ -3122,7 +3122,7 @@ exec_process_entrypoint (libcrun_context_t *context,
   if (UNLIKELY (ret < 0))
     return ret;
 
-  ret = libcrun_set_apparmor_profile (process, err);
+  ret = libcrun_set_apparmor_profile (process, false, err);
   if (UNLIKELY (ret < 0))
     return ret;
 

src/libcrun/linux.c
@@ -3019,10 +3019,10 @@ libcrun_set_selinux_label (runtime_spec_schema_config_schema_process *proc, bool
 }
 
 int
-libcrun_set_apparmor_profile (runtime_spec_schema_config_schema_process *proc, libcrun_error_t *err)
+libcrun_set_apparmor_profile (runtime_spec_schema_config_schema_process *proc, bool now, libcrun_error_t *err)
 {
   if (proc->apparmor_profile)
-    return set_apparmor_profile (proc->apparmor_profile, err);
+    return set_apparmor_profile (proc->apparmor_profile, now, err);
   return 0;
 }
 

src/libcrun/linux.h
@@ -74,7 +74,7 @@ int libcrun_set_caps (runtime_spec_schema_config_schema_process_capabilities *ca
 int libcrun_set_rlimits (runtime_spec_schema_config_schema_process_rlimits_element **rlimits, size_t len,
                          libcrun_error_t *err);
 int libcrun_set_selinux_label (runtime_spec_schema_config_schema_process *proc, bool now, libcrun_error_t *err);
-int libcrun_set_apparmor_profile (runtime_spec_schema_config_schema_process *proc, libcrun_error_t *err);
+int libcrun_set_apparmor_profile (runtime_spec_schema_config_schema_process *proc, bool now, libcrun_error_t *err);
 int libcrun_set_hostname (libcrun_container_t *container, libcrun_error_t *err);
 int libcrun_set_domainname (libcrun_container_t *container, libcrun_error_t *err);
 int libcrun_set_oom (libcrun_container_t *container, libcrun_error_t *err);

src/libcrun/utils.h
@@ -293,7 +293,7 @@ int set_selinux_label (const char *label, bool now, libcrun_error_t *err);
 
 int add_selinux_mount_label (char **ret, const char *data, const char *label, libcrun_error_t *err);
 
-int set_apparmor_profile (const char *profile, libcrun_error_t *err);
+int set_apparmor_profile (const char *profile, bool now, libcrun_error_t *err);
 
 int read_all_fd (int fd, const char *description, char **out, size_t *len, libcrun_error_t *err);
 

commit 26fe1383a05279935e67ee31e7ff10c43e7d87ea

utils: fix applying AppArmor profile
commit 0efbe561afb40023872bcc2db84c701a73dc4dc9 introduced the
regression.  When we change the current AppArmor profile, it is
necessary to use the command "changeprofile" instead of "exec".

Closes: https://github.com/containers/crun/issues/1093

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/utils.c
@@ -850,7 +850,7 @@ set_apparmor_profile (const char *profile, bool now, libcrun_error_t *err)
       const char *fname = now ? "/proc/thread-self/attr/current" : "/proc/thread-self/attr/exec";
       cleanup_free char *buf = NULL;
 
-      xasprintf (&buf, "exec %s", profile);
+      xasprintf (&buf, "%s %s", now ? "changeprofile" : "exec", profile);
 
       ret = write_file_and_check_fs_type (fname, buf, strlen (buf), PROC_SUPER_MAGIC, "procfs",
                                           err);