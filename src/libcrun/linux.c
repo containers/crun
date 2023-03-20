containers/crun

commit 234d77c96fc4aebb46d35fdcf820a00e4dd79e77

linux: precreate devices on the host
when a user namespace is created, the user loses the ability of
creating device nodes thus only bind mounting them from the host is
permitted.  Precreate the device nodes on the host so that we can
honor the permissions and the file mode bits specified in the OCI
spec, create the mount from the host and then bind mount them into the
user namespace.

Closes: https://github.com/containers/crun/pull/1099

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -1470,8 +1470,9 @@ relative_path_under_dev (const char *path)
 }
 
 int
-libcrun_create_dev (libcrun_container_t *container, int devfd, struct device_s *device,
-                    bool binds, bool ensure_parent_dir, libcrun_error_t *err)
+libcrun_create_dev (libcrun_container_t *container, int devfd, int srcfd,
+                    struct device_s *device, bool binds, bool ensure_parent_dir,
+                    libcrun_error_t *err)
 {
   int ret;
   dev_t dev;
@@ -1506,6 +1507,13 @@ libcrun_create_dev (libcrun_container_t *container, int devfd, struct device_s *
           fd = crun_safe_create_and_open_ref_at (false, rootfsfd, rootfs, rootfs_len, rel_path, 0755, err);
           if (UNLIKELY (fd < 0))
             return fd;
+        }
+
+      if (srcfd >= 0)
+        {
+          ret = syscall_move_mount (srcfd, "", fd, "", MOVE_MOUNT_T_EMPTY_PATH | MOVE_MOUNT_F_EMPTY_PATH);
+          if (LIKELY (ret >= 0))
+            return 0;
         }
 
       ret = do_mount (container, fullname, fd, device->path, NULL, MS_BIND | MS_PRIVATE | MS_NOEXEC | MS_NOSUID, NULL, LABEL_MOUNT, err);
@@ -1627,6 +1635,10 @@ create_missing_devs (libcrun_container_t *container, bool binds, libcrun_error_t
   runtime_spec_schema_config_schema *def = container->container_def;
   const char *rootfs = get_private_data (container)->rootfs;
   int rootfsfd = get_private_data (container)->rootfsfd;
+  cleanup_close_map struct libcrun_fd_map *dev_fds = NULL;
+
+  dev_fds = get_private_data (container)->dev_fds;
+  get_private_data (container)->dev_fds = NULL;
 
   devfd = openat (rootfsfd, "dev", O_RDONLY | O_DIRECTORY);
   if (UNLIKELY (devfd < 0))
@@ -1646,7 +1658,7 @@ create_missing_devs (libcrun_container_t *container, bool binds, libcrun_error_t
 
       if (! def->linux->devices[i]->file_mode_present)
         device.mode = 0666;
-      ret = libcrun_create_dev (container, devfd, &device, binds, true, err);
+      ret = libcrun_create_dev (container, devfd, dev_fds->fds[i], &device, binds, true, err);
       if (UNLIKELY (ret < 0))
         return ret;
     }
@@ -1654,7 +1666,7 @@ create_missing_devs (libcrun_container_t *container, bool binds, libcrun_error_t
   for (it = needed_devs; it->path; it++)
     {
       /* make sure the parent directory exists only on the first iteration.  */
-      ret = libcrun_create_dev (container, devfd, it, binds, it == needed_devs, err);
+      ret = libcrun_create_dev (container, devfd, -1, it, binds, it == needed_devs, err);
       if (UNLIKELY (ret < 0))
         return ret;
     }
@@ -3774,6 +3786,62 @@ is_bind_mount (runtime_spec_schema_defs_mount *mnt)
   return false;
 }
 
+static uid_t
+get_id_in_user_namespace (uid_t id, bool is_uid, runtime_spec_schema_config_schema *def)
+{
+  runtime_spec_schema_defs_id_mapping **mappings;
+  size_t len;
+  size_t i;
+
+  mappings = is_uid ? def->linux->uid_mappings : def->linux->gid_mappings;
+  len = is_uid ? def->linux->uid_mappings_len : def->linux->gid_mappings_len;
+
+  for (i = 0; i < len; i++)
+    {
+      if (mappings[i]->container_id <= id
+          && id < mappings[i]->container_id + mappings[i]->size)
+        return id - mappings[i]->container_id + mappings[i]->host_id;
+    }
+
+  return is_uid ? get_overflow_uid () : get_overflow_gid ();
+}
+
+static int
+precreate_device (libcrun_container_t *container, int devs_dirfd, size_t i, libcrun_error_t *err)
+{
+  runtime_spec_schema_config_schema *def = container->container_def;
+  runtime_spec_schema_defs_linux_device *device;
+  uid_t uid = get_overflow_uid ();
+  gid_t gid = get_overflow_gid ();
+  char name[64];
+  mode_t type;
+  dev_t dev;
+  int ret;
+
+  snprintf (name, sizeof (name), "%zu", i);
+
+  device = def->linux->devices[i];
+
+  type = (device->type[0] == 'b') ? S_IFBLK : ((device->type[0] == 'p') ? S_IFIFO : S_IFCHR);
+  dev = makedev (device->major, device->minor);
+
+  ret = mknodat (devs_dirfd, name, device->file_mode | type, dev);
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "mknod `%s`", device->path);
+
+  if (def->linux)
+    {
+      uid = get_id_in_user_namespace (device->uid, true, def);
+      gid = get_id_in_user_namespace (device->gid, false, def);
+    }
+
+  ret = fchownat (devs_dirfd, name, uid, gid, 0); /* lgtm [cpp/toctou-race-condition] */
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "chown `%s`", device->path);
+
+  return get_bind_mount (devs_dirfd, name, err);
+}
+
 static int
 send_mounts (int sync_socket_host, struct libcrun_fd_map *fds, size_t how_many, size_t total, libcrun_error_t *err)
 {
@@ -3839,6 +3907,11 @@ prepare_and_send_dev_mounts (libcrun_container_t *container, int sync_socket_hos
   runtime_spec_schema_config_schema *def = container->container_def;
   cleanup_close_map struct libcrun_fd_map *dev_fds = NULL;
   bool has_userns = (get_private_data (container)->unshare_flags & CLONE_NEWUSER) ? true : false;
+  cleanup_close int current_mountns = -1;
+  cleanup_free char *state_dir = NULL;
+  cleanup_free char *devs_path = NULL;
+  cleanup_close int devs_mountfd = -1;
+  cleanup_close int targetfd = -1;
   size_t how_many = 0;
   size_t i;
   int ret;
@@ -3848,20 +3921,91 @@ prepare_and_send_dev_mounts (libcrun_container_t *container, int sync_socket_hos
 
   dev_fds = make_libcrun_fd_map (def->linux->devices_len);
 
+  if (! has_userns || is_empty_string (container->context->id))
+    return send_mounts (sync_socket_host, dev_fds, how_many, def->linux->devices_len, err);
+
+  state_dir = libcrun_get_state_directory (container->context->state_root, container->context->id);
+  if (state_dir == NULL)
+    return send_mounts (sync_socket_host, dev_fds, how_many, def->linux->devices_len, err);
+
+  ret = append_paths (&devs_path, err, state_dir, "devs", NULL);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  ret = mkdir (devs_path, 0700);
+  if (UNLIKELY (ret < 0) && errno != EEXIST)
+    return crun_make_error (err, errno, "mkdir `%s`", devs_path);
+
+  current_mountns = open ("/proc/self/ns/mnt", O_RDONLY);
+  if (UNLIKELY (current_mountns < 0))
+    return crun_make_error (err, errno, "open `/proc/self/ns/mnt`");
+
+  ret = unshare (CLONE_NEWNS);
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, errno, "unshare `CLONE_NEWNS`");
+
+  ret = mount (NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL);
+  if (UNLIKELY (ret < 0))
+    {
+      ret = crun_make_error (err, errno, "mount `MS_REC | MS_PRIVATE`");
+      goto restore_mountns;
+    }
+
+  devs_mountfd = fsopen_mount ("tmpfs");
+  if (UNLIKELY (devs_mountfd < 0))
+    {
+      ret = crun_make_error (err, errno, "fsopen_mount `tmpfs`");
+      goto restore_mountns;
+    }
+
+  targetfd = open (devs_path, O_DIRECTORY | O_CLOEXEC);
+  if (targetfd < 0)
+    {
+      ret = crun_make_error (err, errno, "open `%s`", devs_path);
+      goto restore_mountns;
+    }
+
+  ret = fs_move_mount_to (devs_mountfd, targetfd, NULL);
+  if (UNLIKELY (ret < 0))
+    {
+      ret = crun_make_error (err, errno, "fs_move_mount_to `%s`", devs_path);
+      goto restore_mountns;
+    }
+
+  close_and_reset (&targetfd);
+
+  targetfd = openat (devs_mountfd, ".", O_DIRECTORY | O_CLOEXEC);
+  if (targetfd < 0)
+    {
+      ret = crun_make_error (err, errno, "open `%s`", devs_path);
+      goto restore_mountns;
+    }
+
   for (i = 0; i < def->linux->devices_len; i++)
     {
-      if (has_userns)
+      ret = precreate_device (container, targetfd, i, err);
+      if (UNLIKELY (ret < 0))
         {
-          dev_fds->fds[i] = -1;
-          if (UNLIKELY (dev_fds->fds[i] < 0))
-            crun_error_release (err);
+          crun_error_release (err);
+          continue;
         }
+
+      dev_fds->fds[i] = ret;
 
       if (dev_fds->fds[i] >= 0)
         how_many++;
     }
 
-  return send_mounts (sync_socket_host, dev_fds, how_many, def->linux->devices_len, err);
+  ret = send_mounts (sync_socket_host, dev_fds, how_many, def->linux->devices_len, err);
+restore_mountns:
+  {
+    int setns_ret;
+
+    setns_ret = setns (current_mountns, CLONE_NEWNS);
+    if (UNLIKELY (setns_ret < 0 && ret >= 0))
+      crun_make_error (err, errno, "setns `CLONE_NEWNS`");
+  }
+  return ret;
 }
 
 static int

src/libcrun/handlers/krun.c
@@ -239,13 +239,13 @@ libkrun_configure_container (void *cookie, enum handler_configure_phase phase,
     return ret;
   is_user_ns = ret;
 
-  ret = libcrun_create_dev (container, devfd, &kvm_device, is_user_ns, true, err);
+  ret = libcrun_create_dev (container, devfd, -1, &kvm_device, is_user_ns, true, err);
   if (UNLIKELY (ret < 0))
     return ret;
 
   if (create_sev)
     {
-      ret = libcrun_create_dev (container, devfd, &sev_device, is_user_ns, true, err);
+      ret = libcrun_create_dev (container, devfd, -1, &sev_device, is_user_ns, true, err);
       if (UNLIKELY (ret < 0))
         return ret;
     }

src/libcrun/linux.h
@@ -109,7 +109,7 @@ int libcrun_create_final_userns (libcrun_container_t *container, libcrun_error_t
 int libcrun_save_external_descriptors (libcrun_container_t *container, pid_t pid, libcrun_error_t *err);
 
 int libcrun_create_dev (libcrun_container_t *container, int devfd,
-                        struct device_s *device, bool binds,
+                        int srcfd, struct device_s *device, bool binds,
                         bool ensure_parent_dir, libcrun_error_t *err);
 
 int parse_idmapped_mount_option (runtime_spec_schema_config_schema *def, bool is_uids, char *option, char **out,

tests/test_devices.py
@@ -18,6 +18,37 @@
 import os
 from tests_utils import *
 
+def test_owner_device():
+    if is_rootless():
+        return 77
+
+    for have_userns in [True, False]:
+        conf = base_config()
+        add_all_namespaces(conf, userns=have_userns)
+        if have_userns:
+            fullMapping = [
+                {
+                    "containerID": 0,
+                    "hostID": 0,
+                    "size": 4294967295
+                }
+            ]
+            conf['linux']['uidMappings'] = fullMapping
+            conf['linux']['gidMappings'] = fullMapping
+
+        conf['process']['args'] = ['/init', 'owner', '/dev/foo']
+        conf['linux']['devices'] = [{"path": "/dev/foo", "type": "b", "major": 1, "minor": 5, "uid": 10, "gid": 11},]
+        try:
+            expected = "10:11"
+            out = run_and_get_output(conf)
+            if expected not in out[0]:
+                sys.stderr.write("wrong file owner, found %s instead of %s with userns=%s" % (out[0], expected, have_userns))
+                return True
+            return False
+        except Exception as e:
+            return -1
+    return 0
+
 def test_deny_devices():
     if is_rootless():
         return 77
@@ -125,6 +156,7 @@ def test_mknod_device():
 
 
 all_tests = {
+    "owner-device" : test_owner_device,
     "deny-devices" : test_deny_devices,
     "allow-device" : test_allow_device,
     "allow-access" : test_allow_access,

commit 11bdc13477478f8b9d0ee9026e0703735adf4bf4

linux: set label for pre-created devices
ensure that the SELinux label for pre-created devices in the tmpfs
matches the label specified in the OCI container configuration.  This
is necessary when using a new user namespace since it prevents the
direct creation of devices in the target environment.

commit 234d77c96fc4aebb46d35fdcf820a00e4dd79e77 introduced the issue.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -3986,6 +3986,8 @@ prepare_and_send_dev_mounts (libcrun_container_t *container, int sync_socket_hos
   cleanup_free char *devs_path = NULL;
   cleanup_close int devs_mountfd = -1;
   cleanup_close int targetfd = -1;
+  const char *context_type = NULL;
+  const char *label = NULL;
   size_t how_many = 0;
   size_t i;
   int ret;
@@ -4025,7 +4027,13 @@ prepare_and_send_dev_mounts (libcrun_container_t *container, int sync_socket_hos
       goto restore_mountns;
     }
 
-  devs_mountfd = fsopen_mount ("tmpfs", NULL, NULL);
+  if (container->container_def->linux && container->container_def->linux->mount_label)
+    {
+      label = container->container_def->linux->mount_label;
+      context_type = get_selinux_context_type (container);
+    }
+
+  devs_mountfd = fsopen_mount ("tmpfs", context_type, label);
   if (UNLIKELY (devs_mountfd < 0))
     {
       ret = crun_make_error (err, errno, "fsopen_mount `tmpfs`");