containers/crun

commit df8ee48f722d8252bf2556b69e1a42c52dcd86d0

criu: check if the criu_join_ns_add function exists
check that the current libcriu library has the criu_join_ns_add
function before attempting to use it.

Closes: https://bugzilla.redhat.com/show_bug.cgi?id=2184221

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/criu.c
@@ -63,7 +63,9 @@ struct libcriu_wrapper_s
   int (*criu_dump) (void);
   int (*criu_get_orphan_pts_master_fd) (void);
   int (*criu_init_opts) (void);
+#  ifdef CRIU_JOIN_NS_SUPPORT
   int (*criu_join_ns_add) (const char *ns, const char *ns_file, const char *extra_opt);
+#  endif
 #  ifdef CRIU_PRE_DUMP_SUPPORT
   int (*criu_feature_check) (struct criu_feature_check *features, size_t size);
   int (*criu_pre_dump) (void);
@@ -135,7 +137,17 @@ load_wrapper (struct libcriu_wrapper_s **wrapper_out, libcrun_error_t *err)
   LOAD_CRIU_FUNCTION (criu_dump);
   LOAD_CRIU_FUNCTION (criu_get_orphan_pts_master_fd);
   LOAD_CRIU_FUNCTION (criu_init_opts);
-  LOAD_CRIU_FUNCTION (criu_join_ns_add);
+
+#  ifdef CRIU_JOIN_NS_SUPPORT
+  /* criu_join_ns_add() API was introduced with CRIU version 3.16.1
+   * Here we check if this API is available at build time to support
+   * compiling with older version of CRIU, and at runtime to support
+   * running crun with older versions of libcriu.so.2.
+   */
+  if (wrapper->criu_check_version (31601) == 1)
+    LOAD_CRIU_FUNCTION (criu_join_ns_add);
+#  endif
+
 #  ifdef CRIU_PRE_DUMP_SUPPORT
   LOAD_CRIU_FUNCTION (criu_feature_check);
   LOAD_CRIU_FUNCTION (criu_pre_dump);
@@ -873,15 +885,6 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
       goto out_umount;
     }
 
-#  ifdef CRIU_JOIN_NS_SUPPORT
-  /* criu_join_ns_add() API was introduced with CRIU version 3.16.1
-   * Here we check if this API is available at build time to support
-   * compiling with older version of CRIU, and at runtime to support
-   * running crun with older versions of libcriu.so.2.
-   */
-  bool join_ns_support = libcriu_wrapper->criu_check_version (31601) == 1;
-#  endif
-
   /* If a namespace defined in config.json we are telling
    * CRIU use that namespace when restoring the process tree.
    *
@@ -915,7 +918,7 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
 #  ifdef CRIU_JOIN_NS_SUPPORT
       if (value == CLONE_NEWTIME && def->linux->namespaces[i]->path != NULL)
         {
-          if (join_ns_support)
+          if (libcriu_wrapper->criu_join_ns_add != NULL)
             libcriu_wrapper->criu_join_ns_add ("time", def->linux->namespaces[i]->path, NULL);
           else
             return crun_make_error (err, 0, "shared time namespace restore is supported in CRIU >= 3.16.1");
@@ -923,7 +926,7 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
 
       if (value == CLONE_NEWIPC && def->linux->namespaces[i]->path != NULL)
         {
-          if (join_ns_support)
+          if (libcriu_wrapper->criu_join_ns_add != NULL)
             libcriu_wrapper->criu_join_ns_add ("ipc", def->linux->namespaces[i]->path, NULL);
           else
             return crun_make_error (err, 0, "shared ipc namespace restore is supported in CRIU >= 3.16.1");
@@ -931,7 +934,7 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
 
       if (value == CLONE_NEWUTS && def->linux->namespaces[i]->path != NULL)
         {
-          if (join_ns_support)
+          if (libcriu_wrapper->criu_join_ns_add != NULL)
             libcriu_wrapper->criu_join_ns_add ("uts", def->linux->namespaces[i]->path, NULL);
           else
             return crun_make_error (err, 0, "shared uts namespace restore is supported in CRIU >= 3.16.1");

commit a717db77908a1100f8b6528a5103dcb2e476742e

criu: fix segfault if CRIU_JOIN_NS_SUPPORT is defined
we cannot use the criu_check_version before calling criu_init_opts.
To avoid the issue, just try to lookup the symbol in any case instead
of checking the CRIU library version.  If it is not available leave
criu_join_ns_add set to NULL.

commit df8ee48f722d8252bf2556b69e1a42c52dcd86d0 introduced the
regression.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/criu.c
@@ -91,17 +91,6 @@ struct libcriu_wrapper_s
   void (*criu_set_work_dir_fd) (int fd);
 };
 
-#  define LOAD_CRIU_FUNCTION(X)                                                                \
-    do                                                                                         \
-      {                                                                                        \
-        wrapper->X = dlsym (wrapper->handle, #X);                                              \
-        if (wrapper->X == NULL)                                                                \
-          {                                                                                    \
-            dlclose (wrapper->handle);                                                         \
-            return crun_make_error (err, 0, "could not find symbol `%s` in `libcriu.so`", #X); \
-          }                                                                                    \
-    } while (0)
-
 static struct libcriu_wrapper_s *libcriu_wrapper;
 
 static inline void
@@ -126,17 +115,28 @@ load_wrapper (struct libcriu_wrapper_s **wrapper_out, libcrun_error_t *err)
 {
   cleanup_free struct libcriu_wrapper_s *wrapper = xmalloc0 (sizeof (*wrapper));
 
+#  define LOAD_CRIU_FUNCTION(X, ALLOW_NULL)                                                    \
+    do                                                                                         \
+      {                                                                                        \
+        wrapper->X = dlsym (wrapper->handle, #X);                                              \
+        if (! ALLOW_NULL && wrapper->X == NULL)                                                \
+          {                                                                                    \
+            dlclose (wrapper->handle);                                                         \
+            return crun_make_error (err, 0, "could not find symbol `%s` in `libcriu.so`", #X); \
+          }                                                                                    \
+    } while (0)
+
   wrapper->handle = dlopen ("libcriu.so.2", RTLD_NOW);
   if (wrapper->handle == NULL)
     return crun_make_error (err, 0, "could not load `libcriu.so.2`");
 
-  LOAD_CRIU_FUNCTION (criu_add_ext_mount);
-  LOAD_CRIU_FUNCTION (criu_add_external);
-  LOAD_CRIU_FUNCTION (criu_add_inherit_fd);
-  LOAD_CRIU_FUNCTION (criu_check_version);
-  LOAD_CRIU_FUNCTION (criu_dump);
-  LOAD_CRIU_FUNCTION (criu_get_orphan_pts_master_fd);
-  LOAD_CRIU_FUNCTION (criu_init_opts);
+  LOAD_CRIU_FUNCTION (criu_add_ext_mount, false);
+  LOAD_CRIU_FUNCTION (criu_add_external, false);
+  LOAD_CRIU_FUNCTION (criu_add_inherit_fd, false);
+  LOAD_CRIU_FUNCTION (criu_check_version, false);
+  LOAD_CRIU_FUNCTION (criu_dump, false);
+  LOAD_CRIU_FUNCTION (criu_get_orphan_pts_master_fd, false);
+  LOAD_CRIU_FUNCTION (criu_init_opts, false);
 
 #  ifdef CRIU_JOIN_NS_SUPPORT
   /* criu_join_ns_add() API was introduced with CRIU version 3.16.1
@@ -144,36 +144,36 @@ load_wrapper (struct libcriu_wrapper_s **wrapper_out, libcrun_error_t *err)
    * compiling with older version of CRIU, and at runtime to support
    * running crun with older versions of libcriu.so.2.
    */
-  if (wrapper->criu_check_version (31601) == 1)
-    LOAD_CRIU_FUNCTION (criu_join_ns_add);
+  LOAD_CRIU_FUNCTION (criu_join_ns_add, true);
 #  endif
 
 #  ifdef CRIU_PRE_DUMP_SUPPORT
-  LOAD_CRIU_FUNCTION (criu_feature_check);
-  LOAD_CRIU_FUNCTION (criu_pre_dump);
+  LOAD_CRIU_FUNCTION (criu_feature_check, false);
+  LOAD_CRIU_FUNCTION (criu_pre_dump, false);
 #  endif
-  LOAD_CRIU_FUNCTION (criu_restore_child);
-  LOAD_CRIU_FUNCTION (criu_set_ext_unix_sk);
-  LOAD_CRIU_FUNCTION (criu_set_file_locks);
-  LOAD_CRIU_FUNCTION (criu_set_freeze_cgroup);
-  LOAD_CRIU_FUNCTION (criu_set_images_dir_fd);
-  LOAD_CRIU_FUNCTION (criu_set_leave_running);
-  LOAD_CRIU_FUNCTION (criu_set_log_file);
-  LOAD_CRIU_FUNCTION (criu_set_log_level);
-  LOAD_CRIU_FUNCTION (criu_set_manage_cgroups);
-  LOAD_CRIU_FUNCTION (criu_set_manage_cgroups_mode);
-  LOAD_CRIU_FUNCTION (criu_set_notify_cb);
-  LOAD_CRIU_FUNCTION (criu_set_orphan_pts_master);
-  LOAD_CRIU_FUNCTION (criu_set_parent_images);
-  LOAD_CRIU_FUNCTION (criu_set_pid);
-  LOAD_CRIU_FUNCTION (criu_set_root);
-  LOAD_CRIU_FUNCTION (criu_set_shell_job);
-  LOAD_CRIU_FUNCTION (criu_set_tcp_established);
-  LOAD_CRIU_FUNCTION (criu_set_track_mem);
-  LOAD_CRIU_FUNCTION (criu_set_work_dir_fd);
+  LOAD_CRIU_FUNCTION (criu_restore_child, false);
+  LOAD_CRIU_FUNCTION (criu_set_ext_unix_sk, false);
+  LOAD_CRIU_FUNCTION (criu_set_file_locks, false);
+  LOAD_CRIU_FUNCTION (criu_set_freeze_cgroup, false);
+  LOAD_CRIU_FUNCTION (criu_set_images_dir_fd, false);
+  LOAD_CRIU_FUNCTION (criu_set_leave_running, false);
+  LOAD_CRIU_FUNCTION (criu_set_log_file, false);
+  LOAD_CRIU_FUNCTION (criu_set_log_level, false);
+  LOAD_CRIU_FUNCTION (criu_set_manage_cgroups, false);
+  LOAD_CRIU_FUNCTION (criu_set_manage_cgroups_mode, false);
+  LOAD_CRIU_FUNCTION (criu_set_notify_cb, false);
+  LOAD_CRIU_FUNCTION (criu_set_orphan_pts_master, false);
+  LOAD_CRIU_FUNCTION (criu_set_parent_images, false);
+  LOAD_CRIU_FUNCTION (criu_set_pid, false);
+  LOAD_CRIU_FUNCTION (criu_set_root, false);
+  LOAD_CRIU_FUNCTION (criu_set_shell_job, false);
+  LOAD_CRIU_FUNCTION (criu_set_tcp_established, false);
+  LOAD_CRIU_FUNCTION (criu_set_track_mem, false);
+  LOAD_CRIU_FUNCTION (criu_set_work_dir_fd, false);
 
   libcriu_wrapper = *wrapper_out = wrapper;
   wrapper = NULL;
+#  undef LOAD_CRIU_FUNCTION
   return 0;
 }
 