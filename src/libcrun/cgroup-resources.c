containers/crun

commit 5a0ede20fdd73c6de09d8c07b4ba2ea410fbb7cb

systemd, cgroupv1: set the cpuset data also on the scope
Closes: https://issues.redhat.com/browse/OCPBUGS-20492

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/cgroup-resources.c
@@ -1016,7 +1016,7 @@ write_cpu_resources (int dirfd_cpu, bool cgroup2, runtime_spec_schema_config_lin
   return 0;
 }
 
-static int
+int
 write_cpuset_resources (int dirfd_cpuset, int cgroup2, runtime_spec_schema_config_linux_resources_cpu *cpu,
                         libcrun_error_t *err)
 {

src/libcrun/cgroup-internal.h
@@ -80,4 +80,8 @@ convert_shares_to_weight (uint64_t shares)
   return (1 + ((shares - 2) * 9999) / 262142);
 }
 
+int initialize_cpuset_subsystem (const char *path, libcrun_error_t *err);
+
+int write_cpuset_resources (int dirfd_cpuset, int cgroup2, runtime_spec_schema_config_linux_resources_cpu *cpu, libcrun_error_t *err);
+
 #endif

src/libcrun/cgroup-setup.c
@@ -121,7 +121,7 @@ initialize_cpuset_subsystem_rec (char *path, size_t path_len, char *cpus, char *
   return 0;
 }
 
-static int
+int
 initialize_cpuset_subsystem (const char *path, libcrun_error_t *err)
 {
   cleanup_free char *tmp_path = xstrdup (path);

src/libcrun/cgroup-systemd.c
@@ -199,6 +199,42 @@ setup_rt_runtime (runtime_spec_schema_config_linux_resources *resources,
 }
 
 static int
+setup_cpuset_for_systemd_v1 (runtime_spec_schema_config_linux_resources *resources, const char *path, libcrun_error_t *err)
+{
+  cleanup_free char *cgroup_path = NULL;
+  int parent;
+  int ret;
+
+  ret = append_paths (&cgroup_path, err, CGROUP_ROOT, "/cpuset", path ? path : "", NULL);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  ret = initialize_cpuset_subsystem (cgroup_path, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  for (parent = 0; parent < 2; parent++)
+    {
+      cleanup_close int dirfd_cpuset = -1;
+      cleanup_free char *path_to_cpuset = NULL;
+
+      ret = append_paths (&path_to_cpuset, err, CGROUP_ROOT "/cpuset", path, (parent ? ".." : NULL), NULL);
+      if (UNLIKELY (ret < 0))
+        return ret;
+
+      dirfd_cpuset = open (path_to_cpuset, O_DIRECTORY | O_RDONLY | O_CLOEXEC);
+      if (UNLIKELY (dirfd_cpuset < 0))
+        return crun_make_error (err, errno, "open `%s`", path_to_cpuset);
+
+      ret = write_cpuset_resources (dirfd_cpuset, false, resources->cpu, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+    }
+
+  return 0;
+}
+
+static int
 systemd_finalize (struct libcrun_cgroup_args *args, char **path_out,
                   int cgroup_mode, const char *suffix, libcrun_error_t *err)
 {
@@ -281,6 +317,10 @@ systemd_finalize (struct libcrun_cgroup_args *args, char **path_out,
         }
 
       ret = setup_rt_runtime (resources, path, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+
+      ret = setup_cpuset_for_systemd_v1 (resources, path, err);
       if (UNLIKELY (ret < 0))
         return ret;
 
@@ -1311,6 +1351,10 @@ libcrun_update_resources_systemd (struct libcrun_cgroup_status *cgroup_status,
   if (cgroup_mode != CGROUP_MODE_UNIFIED)
     {
       ret = setup_rt_runtime (resources, cgroup_status->path, err);
+      if (UNLIKELY (ret < 0))
+        goto exit;
+
+      ret = setup_cpuset_for_systemd_v1 (resources, cgroup_status->path, err);
       if (UNLIKELY (ret < 0))
         goto exit;
     }

commit 6e65f5e50409483da64c78b08b96bf4be5789e7c

cgroup: fix crash on cgroup v1 without cpu resources
fix a crash when running on cgroup v1 and cpu resources are not set in
the configuration file.

commit 5a0ede20fdd73c6de09d8c07b4ba2ea410fbb7cb introduced the
regression.

Closes: https://github.com/containers/crun/issues/1338

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/cgroup-resources.c
@@ -1037,6 +1037,9 @@ write_cpuset_resources (int dirfd_cpuset, int cgroup2, runtime_spec_schema_confi
 {
   int ret;
 
+  if (cpu == NULL)
+    return 0;
+
   if (cpu->cpus)
     {
       ret = write_file_and_check_controllers_at (cgroup2, dirfd_cpuset, "cpuset.cpus", "cpus",