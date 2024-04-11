e6a8d51b958fb8c3ef0f5935a30b9501b29a74d6
Etienne Cordonnier <ecordonnier@snap.com>
2024-04-11 14:48:21 +0200

    libocispec: update
    - update libocispec to https://github.com/containers/libocispec/commit/c715dcdbab8fe592daa8a11cbabc75aa76534c80
    - rename class to _class
    
    Signed-off-by: Etienne Cordonnier <ecordonnier@snap.com>

--- src/libcrun/io_priority.c
+++ src/libcrun/io_priority.c
@@ -66,13 +66,13 @@ libcrun_set_io_priority (pid_t pid, runtime_spec_schema_config_schema_process *p
 
   class_value = -1;
   for (i = 0; classes[i].name; i++)
-    if (strcmp (process->io_priority->class, classes[i].name) == 0)
+    if (strcmp (process->io_priority->_class, classes[i].name) == 0)
       {
         class_value = i;
         break;
       }
   if (UNLIKELY (class_value < 0))
-    return crun_make_error (err, 0, "invalid io priority `%s`", process->io_priority->class);
+    return crun_make_error (err, 0, "invalid io priority `%s`", process->io_priority->_class);
 
   value = IOPRIO_PRIO_VALUE (classes[class_value].value, process->io_priority->priority);
 
