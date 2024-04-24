49090f2f7dd49bb33003cef84943717549b853a5
Sascha Grunert <sgrunert@redhat.com>
2024-04-24 10:17:34 +0200

    Build s390x binaries using musl libc
    Building using musl until NixOS/nixpkgs#306473
    is resolved.
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -1,4 +1,5 @@
-{ pkgs
+{ stdenv
+, pkgs
 , enableCriu
 , enableSystemd
 }:
@@ -17,21 +18,25 @@ with pkgs; stdenv.mkDerivation {
     python3
     which
   ];
-  buildInputs = [
-    glibc
-    glibc.static
-    libcap
-    libseccomp
-    libsystemd
-    yajl
-  ] ++ lib.optionals enableCriu [ criu ];
+  buildInputs =
+    (if stdenv.hostPlatform.isMusl then [
+      argp-standalone
+    ] else [
+      glibc
+      glibc.static
+    ]) ++ [
+      libcap
+      libseccomp
+      libsystemd
+      yajl
+    ] ++ lib.optionals enableCriu [ criu ];
   configureFlags = [ "--enable-static" ] ++ lib.optional (!enableSystemd) [ "--disable-systemd" ];
   prePatch = ''
     export CFLAGS='-static -pthread'
     export LDFLAGS='-s -w -static-libgcc -static'
     export EXTRA_LDFLAGS='-s -w -linkmode external -extldflags "-static -lm"'
     export CRUN_LDFLAGS='-all-static'
-    export LIBS='${lib.optionalString enableCriu "${criu}/lib/libcriu.a"} ${glibc.static}/lib/libc.a ${glibc.static}/lib/libpthread.a ${glibc.static}/lib/librt.a ${lib.getLib libcap}/lib/libcap.a ${lib.getLib libseccomp}/lib/libseccomp.a ${lib.optionalString enableSystemd "${lib.getLib libsystemd}/lib/libsystemd.a"} ${yajl}/lib/libyajl.a'
+    export LIBS='${lib.optionalString enableCriu "${criu}/lib/libcriu.a"} ${if stdenv.hostPlatform.isMusl then "${musl}/lib/libc.a ${musl}/lib/libpthread.a ${musl}/lib/librt.a" else "${glibc.static}/lib/libc.a ${glibc.static}/lib/libpthread.a ${glibc.static}/lib/librt.a"} ${lib.getLib libcap}/lib/libcap.a ${lib.getLib libseccomp}/lib/libseccomp.a ${lib.optionalString enableSystemd "${lib.getLib libsystemd}/lib/libsystemd.a"} ${yajl}/lib/libyajl.a'
   '';
   buildPhase = ''
     patchShebangs .
