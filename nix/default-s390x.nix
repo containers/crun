f916acf0bb24a4c6f00e48b5cbd9e28a40fff9d8
Sascha Grunert <sgrunert@redhat.com>
2024-07-11 09:03:31 +0200

    Enable systemd in s390x builds
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nix/default-s390x.nix
+++ nix/default-s390x.nix
@@ -1,9 +1,6 @@
-{ enableSystemd ? false }:
+{ enableSystemd ? true }:
 (import ./nixpkgs.nix {
   crossSystem = {
-    # TODO: Switch back to glibc when
-    # https://github.com/NixOS/nixpkgs/issues/306473
-    # is resolved.
     config = "s390x-unknown-linux-musl";
   };
   overlays = [ (import ./overlay.nix) ];
