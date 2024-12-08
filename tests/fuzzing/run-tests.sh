9e3615a485fae8948dcd1033ae2e66f251e93054
Jan Palus <jpalus@fastmail.com>
2024-12-08 19:10:42 +0100

    ci: build tests_libcrun_fuzzer before fuzzing
    Signed-off-by: Jan Palus <jpalus@fastmail.com>

--- tests/fuzzing/run-tests.sh
+++ tests/fuzzing/run-tests.sh
@@ -17,6 +17,7 @@ git clean -fdx
 ./autogen.sh
 ./configure --enable-embedded-yajl HFUZZ_CC_UBSAN=1 HFUZZ_CC_ASAN=1 CC=hfuzz-clang CPPFLAGS="-D FUZZER" CFLAGS="-ggdb3 -fsanitize-coverage=trace-pc-guard,trace-cmp,trace-div,indirect-calls"
 make -j "$(nproc)"
+make -j "$(nproc)" tests/tests_libcrun_fuzzer
 
 mkdir rootfs
 mkdir random-data
