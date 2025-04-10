684b2540a3084b22b48013d96c557b7506860647
Giuseppe Scrivano <gscrivan@redhat.com>
2025-04-10 17:26:40 +0200

    linux: fix definition of CLONE_NEWTIME
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- src/libcrun/linux.c
+++ src/libcrun/linux.c
@@ -189,7 +189,7 @@ get_private_data (struct libcrun_container_s *container)
 }
 
 #ifndef CLONE_NEWTIME
-#  define CLONE_NEWTIME 0
+#  define CLONE_NEWTIME 0x00000080
 #endif
 #ifndef CLONE_NEWCGROUP
 #  define CLONE_NEWCGROUP 0
