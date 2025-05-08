containers/crun

commit 90ef9732f8ba1e3c0e359a07ea4a5c08d3e3baa2

criu: create --work-path directory
There is a discrepancy in behavior between how crun and runc
handle --work-path argument:
 - runc creates the directory on checkpoint and restore [1];
 - crun fails with ENOENT if the directory doesn't exist.

Make crun create the work-path directory, fixing the following error:

> crun checkpoint --work-path ./work-dir test_busybox (status=1):
> error opening CRIU work directory `./work-dir`: No such file or directory

Found when running runc's checkpoint.bats tests with crun.

[1]: https://github.com/opencontainers/runc/commit/a8d5fdf1

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

src/libcrun/criu.c
@@ -478,6 +478,10 @@ libcrun_container_checkpoint_linux_criu (libcrun_container_status_t *status, lib
    * crun to set it if the user has not selected a specific directory. */
   if (cr_options->work_path != NULL)
     {
+      ret = mkdir (cr_options->work_path, 0700);
+      if (UNLIKELY ((ret == -1) && (errno != EEXIST)))
+        return crun_make_error (err, errno, "error creating CRIU work directory `%s`", cr_options->image_path);
+
       work_fd = open (cr_options->work_path, O_DIRECTORY | O_CLOEXEC);
       if (UNLIKELY (work_fd == -1))
         return crun_make_error (err, errno, "error opening CRIU work directory `%s`", cr_options->work_path);

commit 157a673d435bc8ae1c6f2ccf41a5ab2eb6370ede

criu checkpoint: error message fixup
Fixes: 90ef973 ("criu: create --work-path directory")
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

src/libcrun/criu.c
@@ -480,7 +480,7 @@ libcrun_container_checkpoint_linux_criu (libcrun_container_status_t *status, lib
     {
       ret = mkdir (cr_options->work_path, 0700);
       if (UNLIKELY ((ret == -1) && (errno != EEXIST)))
-        return crun_make_error (err, errno, "error creating CRIU work directory `%s`", cr_options->image_path);
+        return crun_make_error (err, errno, "error creating CRIU work directory `%s`", cr_options->work_path);
 
       work_fd = open (cr_options->work_path, O_DIRECTORY | O_CLOEXEC);
       if (UNLIKELY (work_fd == -1))