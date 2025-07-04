ce47a219325475942a17af76546a0664d1e8d497
Giuseppe Scrivano <gscrivan@redhat.com>
2025-07-04 11:07:35 +0000

    linux: Add NULL pointer checks to free_remount
    add missing NULL pointer checks to be uniform across the code.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- src/libcrun/linux.c
+++ src/libcrun/linux.c
@@ -867,6 +867,8 @@ pivot_root (const char *new_root, const char *put_old)
 static void
 free_remount (struct remount_s *r)
 {
+  if (r == NULL)
+    return;
   if (r->targetfd >= 0)
     close (r->targetfd);
   free (r->data);
