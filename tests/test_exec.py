containers/crun

commit 7b82568080dfd79f3bf38c47f82e38d4878fb2c9

container: create missing errors
Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

src/libcrun/container.c
@@ -3851,7 +3851,7 @@ libcrun_container_exec_with_options (libcrun_context_t *context, const char *id,
   TEMP_FAILURE_RETRY (close (pipefd0));
   pipefd0 = -1;
   if (ret != 1 || b != '0')
-    ret = -1;
+    ret = crun_make_error (err, 0, "read pipe failed");
   else
     {
       /* Let's receive the seccomp notify fd and handle it as part of wait_for_process().  */
@@ -3863,7 +3863,7 @@ libcrun_container_exec_with_options (libcrun_context_t *context, const char *id,
 
           ret = close_and_reset (&own_seccomp_receiver_fd);
           if (UNLIKELY (ret < 0))
-            return ret;
+            return crun_make_error (err, errno, "close seccomp receiver fd failed");
         }
 
       {

commit e2789c706af21024b3133522d54e08c961dbf655

container: propagate error from exec setup
commit 7b82568080dfd79f3bf38c47f82e38d4878fb2c9 caused a double error
message to be printed when the exec session setup fails.

It is not strictly a bug, since there is nothing wrong to show an
additional error line but that caused Podman to be confused and not
handle the error string correctly.

Partially revert the change, now errors are propagated to the parent
process so that a single error line is printed.

Closes: https://github.com/containers/crun/issues/1823

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/container.c
@@ -2219,6 +2219,7 @@ flush_fd_to_err (libcrun_context_t *context, int terminal_fd)
 {
   char buf[256];
   int flags;
+
   if (terminal_fd < 0 || stderr == NULL)
     return;
 
@@ -3399,7 +3400,7 @@ static int
 exec_process_entrypoint (libcrun_context_t *context,
                          libcrun_container_t *container,
                          runtime_spec_schema_config_schema_process *process,
-                         int pipefd1,
+                         int *pipefd1,
                          int seccomp_fd,
                          int seccomp_receiver_fd,
                          struct custom_handler_instance_s *custom_handler,
@@ -3420,7 +3421,7 @@ exec_process_entrypoint (libcrun_context_t *context,
   container_uid = process->user ? process->user->uid : 0;
   container_gid = process->user ? process->user->gid : 0;
 
-  TEMP_FAILURE_RETRY (read (pipefd1, &own_pid, sizeof (own_pid)));
+  TEMP_FAILURE_RETRY (read (*pipefd1, &own_pid, sizeof (own_pid)));
 
   cwd = process->cwd ? process->cwd : "/";
   if (LIKELY (libcrun_safe_chdir (cwd, err) == 0))
@@ -3554,8 +3555,12 @@ exec_process_entrypoint (libcrun_context_t *context,
         }
     }
 
-  if (UNLIKELY ((! chdir_done) && libcrun_safe_chdir (cwd, err) < 0))
-    libcrun_fail_with_error ((*err)->status, "%s", (*err)->msg);
+  if (! chdir_done)
+    {
+      ret = libcrun_safe_chdir (cwd, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+    }
 
   if (process->no_new_privileges)
     {
@@ -3580,9 +3585,10 @@ exec_process_entrypoint (libcrun_context_t *context,
   if (process->user)
     umask (process->user->umask_present ? process->user->umask : 0022);
 
-  TEMP_FAILURE_RETRY (write (pipefd1, "0", 1));
-  TEMP_FAILURE_RETRY (close (pipefd1));
-  pipefd1 = -1;
+  ret = 0;
+  TEMP_FAILURE_RETRY (write (*pipefd1, &ret, sizeof (ret)));
+  TEMP_FAILURE_RETRY (close (*pipefd1));
+  *pipefd1 = -1;
 
   if (custom_handler)
     {
@@ -3638,7 +3644,7 @@ libcrun_container_exec_with_options (libcrun_context_t *context, const char *id,
   __attribute__ ((unused)) cleanup_process_schema runtime_spec_schema_config_schema_process *process_cleanup = NULL;
   runtime_spec_schema_config_schema_process *process = opts->process;
   struct libcrun_seccomp_gen_ctx_s seccomp_gen_ctx;
-  char b;
+  int ret_from_child = 0;
 
   ret = libcrun_read_container_status (&status, state_root, id, err);
   if (UNLIKELY (ret < 0))
@@ -3793,11 +3799,22 @@ libcrun_container_exec_with_options (libcrun_context_t *context, const char *id,
       TEMP_FAILURE_RETRY (close (pipefd0));
       pipefd0 = -1;
 
-      exec_process_entrypoint (context, container, process, pipefd1, seccomp_fd, seccomp_receiver_fd, custom_handler, err);
+      exec_process_entrypoint (context, container, process, &pipefd1, seccomp_fd, seccomp_receiver_fd, custom_handler, err);
       /* It gets here only on errors.  */
       if (*err)
-        libcrun_fail_with_error ((*err)->status, "%s", (*err)->msg);
-
+        {
+          if (pipefd1 < 0)
+            libcrun_fail_with_error ((*err)->status, "%s", (*err)->msg);
+          else
+            {
+              const char *msg = (*err)->msg;
+              ret = crun_error_get_errno (err);
+              TEMP_FAILURE_RETRY (write (pipefd1, &ret, sizeof (ret)));
+              TEMP_FAILURE_RETRY (write (pipefd1, msg, strlen (msg) + 1));
+              TEMP_FAILURE_RETRY (close (pipefd1));
+              pipefd1 = -1;
+            }
+        }
       _exit (EXIT_FAILURE);
     }
 
@@ -3845,11 +3862,20 @@ libcrun_container_exec_with_options (libcrun_context_t *context, const char *id,
         }
     }
 
-  ret = TEMP_FAILURE_RETRY (read (pipefd0, &b, sizeof (b)));
-  TEMP_FAILURE_RETRY (close (pipefd0));
-  pipefd0 = -1;
-  if (ret != 1 || b != '0')
+  ret = TEMP_FAILURE_RETRY (read (pipefd0, &ret_from_child, sizeof (ret_from_child)));
+  if (ret != sizeof (ret_from_child))
     ret = crun_make_error (err, 0, "read pipe failed");
+  else if (ret_from_child != 0)
+    {
+      cleanup_free char *msg = NULL;
+      size_t len = 0;
+
+      ret = read_all_fd (pipefd0, "error stream", &msg, &len, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+      /* the string from read_all_fd is always NUL terminated.  */
+      ret = crun_make_error (err, ret_from_child, "%s", msg);
+    }
   else
     {
       /* Let's receive the seccomp notify fd and handle it as part of wait_for_process().  */

tests/test_exec.py
@@ -19,6 +19,7 @@ import json
 import os
 import re
 import shutil
+import subprocess
 import tempfile
 from tests_utils import *
 import time
@@ -501,7 +502,41 @@ def test_exec_help():
     out = run_crun_command(["exec", "--help"])
     if "Usage: crun [OPTION...] exec CONTAINER cmd" not in out:
         return -1
-    
+
+    return 0
+
+def test_exec_error_propagation():
+    """Test that exec setup errors are propagated correctly without both chdir and read pipe errors"""
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'pause']
+    add_all_namespaces(conf)
+    cid = None
+    try:
+        _, cid = run_and_get_output(conf, command='run', detach=True)
+        try:
+            out = run_crun_command_raw(["exec", "--cwd", "/invalid/nonexistent/path", cid, "/init", "echo", "test"])
+            return -1
+        except subprocess.CalledProcessError as e:
+            error_msg = e.output.decode('utf-8', errors='ignore')
+            has_chdir_error = "chdir" in error_msg.lower() or "No such file or directory" in error_msg
+            has_read_pipe_error = "read pipe failed" in error_msg
+
+            if has_chdir_error and has_read_pipe_error:
+                sys.stderr.write("# exec error propagation test failed: both chdir and read pipe errors detected\n")
+                sys.stderr.write("# error message: %s\n" % error_msg)
+                return -1
+
+            if not has_chdir_error:
+                sys.stderr.write("# exec error propagation test failed: expected chdir error but got: %s\n" % error_msg)
+                return -1
+
+            return 0
+    finally:
+        if cid is not None:
+            try:
+                run_crun_command(["delete", "-f", cid])
+            except Exception as cleanup_e:
+                sys.stderr.write("# warning: failed to cleanup container %s: %s\n" % (cid, str(cleanup_e)))
     return 0
 
 all_tests = {
@@ -520,6 +555,7 @@ all_tests = {
     "exec-cpu-affinity": test_exec_cpu_affinity,
     "exec-getpgrp": test_exec_getpgrp,
     "exec-help" : test_exec_help,
+    "exec-error-propagation" : test_exec_error_propagation,
 }
 
 if __name__ == "__main__":