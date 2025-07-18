containers/crun

commit ab64a5cb5e141edea1a41a3c3e40fb804e1880b8

linux: fix recvfrom error handling
Fixes: https://github.com/containers/crun/issues/1531

Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

src/libcrun/linux.c
@@ -5545,8 +5545,7 @@ libcrun_configure_network (libcrun_container_t *container, libcrun_error_t *err)
           /* The first 4 bytes in the data are the negative error code
              in native endianness.  */
           errno = -(*(int32_t *) (buf + sizeof (struct nlmsghdr)));
-          if (UNLIKELY (errno < 0))
-            return crun_make_error (err, errno, "recvfrom(PF_NETLINK)");
+          return crun_make_error (err, errno, "recvfrom(PF_NETLINK)");
         }
     }
 

commit d30d1512e1b25595c6fcc13ef0a1609705b45e38

linux: fix regression in libcrun_configure_network
commit ab64a5cb5e141edea1a41a3c3e40fb804e1880b8 introduced the
regression.

Closes: https://github.com/containers/crun/issues/1828

Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

src/libcrun/linux.c
@@ -5783,10 +5783,10 @@ libcrun_configure_network (libcrun_container_t *container, libcrun_error_t *err)
 
       if (hdr_recv->nlmsg_type == NLMSG_ERROR)
         {
-          /* The first 4 bytes in the data are the negative error code
-             in native endianness.  */
-          errno = -(*(int32_t *) (buf + sizeof (struct nlmsghdr)));
-          return crun_make_error (err, errno, "recvfrom PF_NETLINK");
+          struct nlmsgerr *err_data = (struct nlmsgerr *) NLMSG_DATA (hdr_recv);
+          /* err_data->error set to 0 means success acknowledgement due to NLM_F_ACK  */
+          if (err_data->error < 0)
+            return crun_make_error (err, -err_data->error, "netlink error while configuring network");
         }
     }
 