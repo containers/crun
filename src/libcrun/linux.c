containers/crun

commit 4a27212af83e07505d945aec54a9cf3abdeabd7e

linux: move prepare_mount to its only caller
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -4208,42 +4208,6 @@ send_mounts (int sync_socket_host, struct libcrun_fd_map *fds, size_t how_many, 
 }
 
 static int
-prepare_mount (libcrun_container_t *container, runtime_spec_schema_defs_mount *mnt, bool has_userns, pid_t pid, int *out_fd, libcrun_error_t *err)
-{
-  runtime_spec_schema_config_schema *def = container->container_def;
-  cleanup_close int fd = -1;
-  bool recursive = false;
-  int ret;
-
-  if (! has_userns)
-    {
-      int is_in_userns;
-
-      is_in_userns = check_running_in_user_namespace (err);
-      if (UNLIKELY (is_in_userns < 0))
-        return is_in_userns;
-
-      if (is_in_userns > 0)
-        has_userns = true;
-    }
-
-  ret = maybe_get_idmapped_mount (def, mnt, pid, &fd, err);
-  if (UNLIKELY (ret < 0))
-    return ret;
-
-  if (fd < 0 && has_userns && is_bind_mount (mnt, &recursive))
-    {
-      fd = get_bind_mount (-1, mnt->source, recursive, false, err);
-      if (UNLIKELY (fd < 0))
-        crun_error_release (err);
-    }
-
-  *out_fd = fd;
-  fd = -1;
-  return 0;
-}
-
-static int
 prepare_and_send_mount_mounts (libcrun_container_t *container, pid_t pid, int sync_socket_host, libcrun_error_t *err)
 {
   runtime_spec_schema_config_schema *def = container->container_def;
@@ -4258,20 +4222,31 @@ prepare_and_send_mount_mounts (libcrun_container_t *container, pid_t pid, int sy
 
   mount_fds = make_libcrun_fd_map (def->mounts_len);
 
-  for (i = 0; i < def->mounts_len; i++)
+  if (has_userns)
     {
-      int mount_fd = -1;
+      for (i = 0; i < def->mounts_len; i++)
+        {
+          bool recursive = false;
+          int mount_fd = -1;
 
-      ret = prepare_mount (container, def->mounts[i], has_userns, pid, &mount_fd, err);
-      if (UNLIKELY (ret < 0))
-        return ret;
+          ret = maybe_get_idmapped_mount (def, def->mounts[i], pid, &mount_fd, err);
+          if (UNLIKELY (ret < 0))
+            return ret;
 
-      if (mount_fd >= 0)
-        how_many++;
+          if (mount_fd < 0 && is_bind_mount (def->mounts[i], &recursive))
+            {
+              /* If the bind mount failed, do not fail here, but attempt to create it from within the container.  */
+              mount_fd = get_bind_mount (-1, def->mounts[i]->source, recursive, false, err);
+              if (UNLIKELY (mount_fd < 0))
+                crun_error_release (err);
+            }
 
-      mount_fds->fds[i] = mount_fd;
+          if (mount_fd >= 0)
+            how_many++;
+
+          mount_fds->fds[i] = mount_fd;
+        }
     }
-
   return send_mounts (sync_socket_host, mount_fds, how_many, def->mounts_len, err);
 }
 

commit e486eeabda271b7b5f1cd73c9ecfe2848f26f428

linux: fix regression mounting within userns
commit 4a27212af83e07505d945aec54a9cf3abdeabd7e introduced the
regression.

Detect when running inside a user namespace and treat the mounts in
the same way as they would be treated if creating a new user
namespace.

Closes: https://github.com/containers/crun/issues/1855

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/linux.c
@@ -4462,6 +4462,17 @@ prepare_and_send_mount_mounts (libcrun_container_t *container, pid_t pid, int sy
 
   mount_fds = make_libcrun_fd_map (def->mounts_len);
 
+  /* If the container is already running in a user namespace, apply the same logic as if a new
+     user namespace was created as part of the container itself.  */
+  if (! has_userns)
+    {
+      int is_in_userns = check_running_in_user_namespace (err);
+      if (UNLIKELY (is_in_userns < 0))
+        return is_in_userns;
+
+      has_userns = is_in_userns > 0;
+    }
+
   for (i = 0; i < def->mounts_len; i++)
     {
       bool recursive = false;