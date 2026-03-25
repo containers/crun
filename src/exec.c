containers/crun

commit 85d4db3d8b27c9ca606eea5f007c58a752ae77aa

crun: check for integer overflow
validate that the stroll returned value can fit into an integer.

Closes: https://github.com/containers/crun/issues/1604

Reported-by: Pavel Nekrasov <p.nekrasov@fobos-nt.ru>
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/exec.c
@@ -23,6 +23,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <limits.h>
 
 #include "crun.h"
 #include "libcrun/container.h"
@@ -151,7 +152,7 @@ parse_opt (int key, char *arg, struct argp_state *state)
       break;
 
     case OPTION_PRESERVE_FDS:
-      exec_options.preserve_fds = strtoul (argp_mandatory_argument (arg, state), NULL, 10);
+      exec_options.preserve_fds = parse_int_or_fail (argp_mandatory_argument (arg, state), "preserve-fds");
       break;
 
     case OPTION_CGROUP:
@@ -203,27 +204,34 @@ make_oci_process_user (const char *userspec)
 {
   runtime_spec_schema_config_schema_process_user *u;
   char *endptr = NULL;
+  long long l;
 
   if (userspec == NULL)
     return NULL;
 
   u = xmalloc0 (sizeof (runtime_spec_schema_config_schema_process_user));
   errno = 0;
-  u->uid = strtol (userspec, &endptr, 10);
+  l = strtoll (userspec, &endptr, 10);
   if (errno == ERANGE)
     libcrun_fail_with_error (0, "invalid UID specified");
   if (*endptr == '\0')
     return u;
   if (*endptr != ':')
     libcrun_fail_with_error (0, "invalid USERSPEC specified");
+  if (l < INT_MIN || l > INT_MAX)
+    libcrun_fail_with_error (0, "invalid UID specified");
+
+  u->uid = (int) l;
 
   errno = 0;
-  u->gid = strtol (endptr + 1, &endptr, 10);
+  l = strtoll (endptr + 1, &endptr, 10);
   if (errno == ERANGE)
+    libcrun_fail_with_error (0, "invalid GID specified");
+  if (l < INT_MIN || l > INT_MAX)
     libcrun_fail_with_error (0, "invalid GID specified");
   if (*endptr != '\0')
     libcrun_fail_with_error (0, "invalid USERSPEC specified");
-
+  u->gid = (int) l;
   return u;
 }
 

src/create.c
@@ -80,7 +80,7 @@ parse_opt (int key, char *arg, struct argp_state *state)
       break;
 
     case OPTION_PRESERVE_FDS:
-      crun_context.preserve_fds = strtoul (argp_mandatory_argument (arg, state), NULL, 10);
+      crun_context.preserve_fds = parse_int_or_fail (argp_mandatory_argument (arg, state), "preserve-fds");
       break;
 
     case OPTION_NO_SUBREAPER:
@@ -166,7 +166,7 @@ crun_command_create (struct crun_global_arguments *global_args, int argc, char *
   crun_context.bundle = bundle;
   if (getenv ("LISTEN_FDS"))
     {
-      crun_context.listen_fds = strtoll (getenv ("LISTEN_FDS"), NULL, 10);
+      crun_context.listen_fds = parse_int_or_fail (getenv ("LISTEN_FDS"), "LISTEN_FDS");
       crun_context.preserve_fds += crun_context.listen_fds;
     }
 

src/crun.c
@@ -22,6 +22,8 @@
 #include <argp.h>
 #include <string.h>
 #include <libgen.h>
+#include <errno.h>
+#include <limits.h>
 
 #ifdef HAVE_DLOPEN
 #  include <dlfcn.h>
@@ -371,6 +373,24 @@ argp_mandatory_argument (char *arg, struct argp_state *state)
   if (arg)
     return arg;
   return state->argv[state->next++];
+}
+
+int
+parse_int_or_fail (const char *str, const char *kind)
+{
+  char *endptr = NULL;
+  long long l;
+
+  errno = 0;
+  l = strtoll (str, &endptr, 10);
+  if (errno != 0)
+    libcrun_fail_with_error (errno, "invalid value for `%s`", kind);
+  if (endptr != NULL && *endptr != '\0')
+    libcrun_fail_with_error (EINVAL, "invalid value for `%s`", kind);
+  if (l < INT_MIN || l > INT_MAX)
+    libcrun_fail_with_error (ERANGE, "invalid value for `%s`", kind);
+
+  return (int) l;
 }
 
 static struct argp argp = { options, parse_opt, args_doc, doc, NULL, NULL, NULL };

src/crun.h
@@ -37,6 +37,7 @@ struct crun_global_arguments
 };
 
 char *argp_mandatory_argument (char *arg, struct argp_state *state);
+int parse_int_or_fail (const char *str, const char *kind);
 int init_libcrun_context (libcrun_context_t *con, const char *id, struct crun_global_arguments *glob,
                           libcrun_error_t *err);
 void crun_assert_n_args (int n, int min, int max);

src/run.c
@@ -93,7 +93,7 @@ parse_opt (int key, char *arg, struct argp_state *state)
       break;
 
     case OPTION_PRESERVE_FDS:
-      crun_context.preserve_fds = strtoll (argp_mandatory_argument (arg, state), NULL, 10);
+      crun_context.preserve_fds = parse_int_or_fail (argp_mandatory_argument (arg, state), "preserve-fds");
       break;
 
     case OPTION_NO_SUBREAPER:
@@ -177,7 +177,7 @@ crun_command_run (struct crun_global_arguments *global_args, int argc, char **ar
   crun_context.bundle = bundle;
   if (getenv ("LISTEN_FDS"))
     {
-      crun_context.listen_fds = strtoll (getenv ("LISTEN_FDS"), NULL, 10);
+      crun_context.listen_fds = parse_int_or_fail (getenv ("LISTEN_FDS"), "LISTEN_FDS");
       crun_context.preserve_fds += crun_context.listen_fds;
     }
 

commit 1bd7f42446999b0e76bc3d575392e05c943b0b01

exec: fix CVE-2026-30892
Fix parsing of the -u (--user) option for "crun exec".

Before:
Value "1" is interpreted as UID=0 GID=0

After:
Value "1" is interpreted as UID=1 GID=0

Commit 85d4db3d8b27c9ca606eea5f007c58a752ae77aa introduced the
regression.

Reference:
https://github.com/containers/crun/security/advisories/GHSA-4vg2-xjqj-7chj

Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

src/exec.c
@@ -214,14 +214,13 @@ make_oci_process_user (const char *userspec)
   l = strtoll (userspec, &endptr, 10);
   if (errno == ERANGE)
     libcrun_fail_with_error (0, "invalid UID specified");
+  if (l < INT_MIN || l > INT_MAX)
+    libcrun_fail_with_error (0, "invalid UID specified");
+  u->uid = (int) l;
   if (*endptr == '\0')
     return u;
   if (*endptr != ':')
     libcrun_fail_with_error (0, "invalid USERSPEC specified");
-  if (l < INT_MIN || l > INT_MAX)
-    libcrun_fail_with_error (0, "invalid UID specified");
-
-  u->uid = (int) l;
 
   errno = 0;
   l = strtoll (endptr + 1, &endptr, 10);