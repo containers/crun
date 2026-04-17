containers/crun

commit c4e3fd228696d411fc9cd16e5a80eb43fddb8e1b

linux: set MS_PRIVATE on detached mounts from get_bind_mount()
Detached mounts from open_tree(OPEN_TREE_CLONE) do not inherit
propagation from the parent mount tree, so they keep shared
propagation when root_propagation_private skips the MS_PRIVATE
call in do_mount().  Set attr.propagation = MS_PRIVATE in
get_bind_mount() to prevent mount leaks into the host namespace.

Add a regression test that verifies bind mounts inside the
container have private propagation.

Fixes: #2059
Signed-off-by: Jindrich Novy <jnovy@redhat.com>

src/libcrun/linux.c
@@ -311,6 +311,11 @@ get_bind_mount (int dirfd, const char *src, bool recursive, bool rdonly, bool no
   if (rdonly)
     attr.attr_set = MS_RDONLY;
 
+  /* Detached mounts created by open_tree(OPEN_TREE_CLONE) do not inherit
+     the propagation type from the parent mount tree.  Always set MS_PRIVATE
+     to prevent mount events from leaking back to the host namespace.  */
+  attr.propagation = MS_PRIVATE;
+
   errno = 0;
   open_tree_fd = syscall_open_tree (dirfd, src,
                                     AT_NO_AUTOMOUNT | OPEN_TREE_CLOEXEC

tests/test_mounts.py
@@ -952,6 +952,37 @@ def test_annotation_mount_context_type():
 
     return 0
 
+def test_mount_propagation_private():
+    """Verify bind mounts have private propagation and don't leak to the host.
+
+    Regression test for https://github.com/containers/crun/issues/2059.
+    Detached mounts from open_tree(OPEN_TREE_CLONE) do not inherit
+    propagation from the parent mount tree, so they must explicitly
+    set MS_PRIVATE to prevent mount events from leaking back.
+    """
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'cat', '/proc/self/mountinfo']
+    add_all_namespaces(conf)
+    conf['linux']['rootfsPropagation'] = 'rprivate'
+    mount_opt = {"destination": "/mnt", "type": "bind", "source": get_tests_root(),
+                 "options": ["bind", "rprivate"]}
+    conf['mounts'].append(mount_opt)
+    out, _ = run_and_get_output(conf, hide_stderr=True)
+    for line in out.splitlines():
+        if '/mnt' not in line:
+            continue
+        # mountinfo optional fields (between the hyphen separator and the
+        # mount ID fields) contain "shared:N" for shared propagation.
+        # With private propagation there should be no "shared:" tag.
+        if 'shared:' in line:
+            logger.info("bind mount at /mnt has shared propagation, expected private")
+            logger.info("mountinfo line: %s", line)
+            return -1
+        return 0
+    logger.info("/mnt not found in mountinfo")
+    logger.info("mountinfo output: %s", out)
+    return -1
+
 all_tests = {
     "mount-ro" : test_mount_ro,
     "mount-rro" : test_mount_rro,
@@ -989,6 +1020,7 @@ all_tests = {
     "mount-add-remove-mounts": test_add_remove_mounts,
     "mount-help": test_mount_help,
     "annotation-mount-context-type": test_annotation_mount_context_type,
+    "mount-propagation-private": test_mount_propagation_private,
 }
 
 if __name__ == "__main__":

commit 1c5c2f7dd778210af6e8f01d23d0b57358aff5bc

linux: pass correct propagation to get_bind_mount
commit c4e3fd228696d411fc9cd16e5a80eb43fddb8e1b introduced this
regression.

get_bind_mount() used mount_setattr() with hardcoded MS_PRIVATE,
which destroyed propagation peer groups for all bind mounts.  Add a
propagation parameter to get_bind_mount() so callers can specify the
correct propagation type.

Closes: https://github.com/containers/crun/issues/2066

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/libcrun/cloned_binary.c
@@ -370,7 +370,7 @@ static int seal_execfd(int *fd, int fdtype)
 static int try_bindfd_mount_api(void)
 {
 	libcrun_error_t err;
-	int mountfd = get_bind_mount (-1, "/proc/self/exe", false, true, false, &err);
+	int mountfd = get_bind_mount (-1, "/proc/self/exe", false, true, false, MS_PRIVATE, &err);
 	if (mountfd < 0) {
 		crun_error_release (&err);
 		return -1;

src/libcrun/linux.c
@@ -298,7 +298,7 @@ do_mount_setattr (bool recursive, const char *target, int targetfd, uint64_t cle
 }
 
 int
-get_bind_mount (int dirfd, const char *src, bool recursive, bool rdonly, bool nofollow, libcrun_error_t *err)
+get_bind_mount (int dirfd, const char *src, bool recursive, bool rdonly, bool nofollow, unsigned long propagation, libcrun_error_t *err)
 {
   cleanup_close int open_tree_fd = -1;
   struct mount_attr_s attr = {
@@ -310,10 +310,7 @@ get_bind_mount (int dirfd, const char *src, bool recursive, bool rdonly, bool no
   if (rdonly)
     attr.attr_set = MS_RDONLY;
 
-  /* Detached mounts created by open_tree(OPEN_TREE_CLONE) do not inherit
-     the propagation type from the parent mount tree.  Always set MS_PRIVATE
-     to prevent mount events from leaking back to the host namespace.  */
-  attr.propagation = MS_PRIVATE;
+  attr.propagation = propagation;
 
   errno = 0;
   open_tree_fd = syscall_open_tree (dirfd, src,
@@ -1014,7 +1011,7 @@ mount_masked_dir (libcrun_container_t *container, int pathfd, const char *rel_pa
   {
     cleanup_close int mountfd = -1;
 
-    mountfd = get_bind_mount (-1, proc_fd_path, false, true, false, &tmp_err);
+    mountfd = get_bind_mount (-1, proc_fd_path, false, true, false, MS_PRIVATE, &tmp_err);
     if (mountfd >= 0)
       {
         ret = fs_move_mount_to (mountfd, pathfd, NULL);
@@ -1077,7 +1074,7 @@ do_masked_or_readonly_path (libcrun_container_t *container, const char *rel_path
          to inherit parent mount flags.  */
       if (! keep_flags)
         {
-          mountfd = get_bind_mount (-1, source_buffer, true, true, false, err);
+          mountfd = get_bind_mount (-1, source_buffer, true, true, false, MS_PRIVATE, err);
           if (mountfd >= 0)
             ret = fs_move_mount_to (mountfd, pathfd, NULL);
           else
@@ -1121,7 +1118,7 @@ do_masked_or_readonly_path (libcrun_container_t *container, const char *rel_path
         {
           cleanup_close int mountfd = -1;
 
-          mountfd = get_bind_mount (-1, "/dev/null", false, true, false, err);
+          mountfd = get_bind_mount (-1, "/dev/null", false, true, false, MS_PRIVATE, err);
           if (mountfd >= 0)
             ret = fs_move_mount_to (mountfd, pathfd, NULL);
 
@@ -1264,7 +1261,7 @@ do_mount (libcrun_container_t *container, const char *source, int targetfd,
                       return do_masked_or_readonly_path (container, "/sys/fs/cgroup", false, false, err);
                     }
 
-                  mountfd = get_bind_mount (-1, "/sys", true, true, false, err);
+                  mountfd = get_bind_mount (-1, "/sys", true, true, false, MS_PRIVATE, err);
                   if (UNLIKELY (mountfd < 0))
                     return mountfd;
 
@@ -1705,7 +1702,7 @@ libcrun_create_dev (libcrun_container_t *container, int devfd, int srcfd,
         cleanup_close int mountfd = -1;
 
         /* Try open_tree+mount_setattr to apply flags atomically.  */
-        mountfd = get_bind_mount (-1, fullname, false, false, false, err);
+        mountfd = get_bind_mount (-1, fullname, false, false, false, MS_PRIVATE, err);
         if (mountfd >= 0)
           {
             ret = do_mount_setattr (false, normalized_path, mountfd, 0, MS_NOSUID | MS_NOEXEC, err);
@@ -2321,7 +2318,7 @@ process_single_mount (libcrun_container_t *container, const char *rootfs,
 
       if (S_ISLNK (src_mode) && (extra_flags & OPTION_DEST_NOFOLLOW) && source_mountfd < 0)
         {
-          ret = get_bind_mount (AT_FDCWD, mount->source, true, true, extra_flags & OPTION_SRC_NOFOLLOW, err);
+          ret = get_bind_mount (AT_FDCWD, mount->source, true, true, extra_flags & OPTION_SRC_NOFOLLOW, MS_PRIVATE, err);
           if (UNLIKELY (ret < 0))
             return ret;
 
@@ -4541,7 +4538,7 @@ precreate_device (libcrun_container_t *container, int devs_dirfd, size_t i, libc
   if (UNLIKELY (ret < 0))
     return crun_make_error (err, errno, "chown `%s`", device->path);
 
-  return get_bind_mount (devs_dirfd, name, false, false, false, err);
+  return get_bind_mount (devs_dirfd, name, false, false, false, MS_PRIVATE, err);
 }
 
 static int
@@ -4606,8 +4603,15 @@ prepare_and_send_mount_mounts (libcrun_container_t *container, pid_t pid, int sy
       /* If the mount has no mappings and there is not a different user namespace, create the mount later as part of the container setup.  */
       if (mount_fd < 0 && (has_mappings || has_userns) && is_bind_mount (def->mounts[i], &recursive, &nofollow))
         {
+          unsigned long propagation = 0;
+
+          if (def->linux && def->linux->rootfs_propagation)
+            propagation = get_mount_flags (def->linux->rootfs_propagation, 0, NULL, NULL, NULL, NULL) & ALL_PROPAGATIONS_NO_REC;
+          if (propagation == 0)
+            propagation = MS_PRIVATE;
+
           /* If the bind mount failed, do not fail here, but attempt to create it from within the container.  */
-          mount_fd = get_bind_mount (-1, def->mounts[i]->source, recursive, false, nofollow, err);
+          mount_fd = get_bind_mount (-1, def->mounts[i]->source, recursive, false, nofollow, propagation, err);
           if (UNLIKELY (mount_fd < 0))
             crun_error_release (err);
         }
@@ -6426,6 +6430,16 @@ libcrun_make_runtime_mounts (libcrun_container_t *container, libcrun_container_s
       if (UNLIKELY (ret < 0))
         return ret;
 
+      if (mounts[i]->options == NULL)
+        flags = get_default_flags (container, mounts[i]->destination, &data);
+      else
+        {
+          size_t j;
+
+          for (j = 0; j < mounts[i]->options_len; j++)
+            flags |= get_mount_flags_or_option (mounts[i]->options[j], flags, &extra_flags, &data, &rec_clear, &rec_set);
+        }
+
       if (fds->fds[i] < 0)
         {
           bool recursive = false;
@@ -6433,7 +6447,11 @@ libcrun_make_runtime_mounts (libcrun_container_t *container, libcrun_container_s
 
           if (is_bind_mount (mounts[i], &recursive, &nofollow))
             {
-              fds->fds[i] = get_bind_mount (-1, mounts[i]->source, recursive, false, nofollow, err);
+              unsigned long propagation = flags & ALL_PROPAGATIONS_NO_REC;
+              if (propagation == 0)
+                propagation = MS_PRIVATE;
+
+              fds->fds[i] = get_bind_mount (-1, mounts[i]->source, recursive, false, nofollow, propagation, err);
               if (UNLIKELY (fds->fds[i] < 0))
                 return fds->fds[i];
             }
@@ -6443,16 +6461,6 @@ libcrun_make_runtime_mounts (libcrun_container_t *container, libcrun_container_s
               if (UNLIKELY (ret < 0))
                 return ret;
             }
-        }
-
-      if (mounts[i]->options == NULL)
-        flags = get_default_flags (container, mounts[i]->destination, &data);
-      else
-        {
-          size_t j;
-
-          for (j = 0; j < mounts[i]->options_len; j++)
-            flags |= get_mount_flags_or_option (mounts[i]->options[j], flags, &extra_flags, &data, &rec_clear, &rec_set);
         }
 
       ret = do_mount_setattr (false, mounts[i]->destination, fds->fds[i], 0, flags, err);

src/libcrun/linux.h
@@ -139,7 +139,7 @@ int libcrun_update_intel_rdt (const char *ctr_name, libcrun_container_t *contain
 
 int libcrun_safe_chdir (const char *path, libcrun_error_t *err);
 
-int get_bind_mount (int dirfd, const char *src, bool recursive, bool rdonly, bool nofollow, libcrun_error_t *err);
+int get_bind_mount (int dirfd, const char *src, bool recursive, bool rdonly, bool nofollow, unsigned long propagation, libcrun_error_t *err);
 
 bool is_bind_mount (runtime_spec_schema_defs_mount *mnt, bool *recursive, bool *src_nofollow);
 

tests/test_mounts.py
@@ -1035,6 +1035,37 @@ def test_mount_no_leak_to_host():
     finally:
         shutil.rmtree(source_dir, ignore_errors=True)
 
+def test_mount_propagation_slave():
+    """Verify bind mounts with rslave propagation have master peer group.
+
+    The mount must preserve its propagation peer group so that rslave propagation
+    can be applied later.
+    """
+    if is_rootless():
+        return (77, "requires root privileges")
+    conf = base_config()
+    conf['process']['args'] = ['/init', 'cat', '/proc/self/mountinfo']
+    add_all_namespaces(conf, userns=True)
+    conf['linux']['rootfsPropagation'] = 'rslave'
+    mount_opt = {"destination": "/mnt", "type": "bind", "source": "/usr",
+                 "options": ["bind", "rslave"]}
+    conf['mounts'].append(mount_opt)
+    out, _ = run_and_get_output(conf, hide_stderr=True)
+    for line in out.splitlines():
+        fields = line.split()
+        if len(fields) < 5 or fields[4] != '/mnt':
+            continue
+        # With rslave propagation, the mount should have a master peer group
+        # indicated by "master:N" in the optional fields.
+        if 'master:' in line:
+            return 0
+        logger.info("bind mount at /mnt has no master peer group, expected rslave propagation")
+        logger.info("mountinfo line: %s", line)
+        return -1
+    logger.info("/mnt not found in mountinfo")
+    logger.info("mountinfo output: %s", out)
+    return -1
+
 all_tests = {
     "mount-ro" : test_mount_ro,
     "mount-rro" : test_mount_rro,
@@ -1074,6 +1105,7 @@ all_tests = {
     "annotation-mount-context-type": test_annotation_mount_context_type,
     "mount-propagation-private": test_mount_propagation_private,
     "mount-no-leak-to-host": test_mount_no_leak_to_host,
+    "mount-propagation-slave": test_mount_propagation_slave,
 }
 
 if __name__ == "__main__":